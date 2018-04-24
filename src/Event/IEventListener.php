26e16d2499b37b72ebb455683d37f043f08b13ee
宇润 <admin@yurunsoft.com>
2018-04-24 14:45:15 +0800

    完善注释

--- src/Event/IEventListener.php
+++ src/Event/IEventListener.php
@@ -1,7 +1,15 @@
 <?php
 namespace Imi\Event;
 
+/**
+ * 监听类接口
+ */
 interface IEventListener
 {
+	/**
+	 * 事件处理方法
+	 * @param EventParam $e
+	 * @return void
+	 */
 	public function handle(EventParam $e);
 }
