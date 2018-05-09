200e7f918516533ce2d0590a65db6df55b488051
宇润 <admin@yurunsoft.com>
2018-05-09 13:35:31 +0800

    新增全局事件监听注解支持

---
+++ src/Bean/Annotation/Listener.php
@@ -0,0 +1,29 @@
+<?php
+namespace Imi\Bean\Annotation;
+
+/**
+ * 类事件监听
+ * @Annotation
+ * @Target("CLASS")
+ * @Parser("Imi\Bean\Parser\ListenerParser")
+ */
+class Listener extends Base
+{
+	/**
+	 * 只传一个参数时的参数名
+	 * @var string
+	 */
+	protected $defaultFieldName = 'eventName';
+
+	/**
+	 * 事件名
+	 * @var string
+	 */
+	public $eventName;
+	
+	/**
+	 * 优先级，越大越先执行
+	 * @var int
+	 */
+	public $priority = 0;
+}
