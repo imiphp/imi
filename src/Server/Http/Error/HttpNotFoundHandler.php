1b75828b02c14d8fa158ffd2c51ce0a3d3551d99
宇润 <admin@yurunsoft.com>
2018-08-01 15:28:00 +0800

    新增http not found处理器

---
+++ src/Server/Http/Error/HttpNotFoundHandler.php
@@ -0,0 +1,34 @@
+<?php
+namespace Imi\Server\Http\Error;
+
+use Imi\App;
+use Imi\Bean\Annotation\Bean;
+use Imi\Util\Http\Consts\StatusCode;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * http 未匹配路由时的处理器
+ * @Bean("HttpNotFoundHandler")
+ */
+class HttpNotFoundHandler implements IHttpNotFoundHandler
+{
+	/**
+	 * 处理器类名，如果为null则使用默认处理
+	 *
+	 * @var string
+	 */
+	protected $handler = null;
+
+	public function handle(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+	{
+		if(null === $this->handler)
+		{
+			return $response->withStatus(StatusCode::NOT_FOUND);
+		}
+		else
+		{
+			return App::getBean($this->handler)->handle($request, $response);
+		}
+	}
+}
