904bef942f97fef68d06a26c0d1f1c617696c587
宇润 <admin@yurunsoft.com>
2018-08-06 09:32:05 +0800

    新增事件IMI.MAIN_SERVER.WORKER.START.APP

--- src/Listener/AfterWorkerStart.php
+++ src/Listener/AfterWorkerStart.php
@@ -6,6 +6,7 @@ use Imi\Server\Event\Param\WorkerStartEventParam;
 use Imi\Server\Event\Listener\IWorkerStartEventListener;
 use Imi\Worker;
 use Imi\Util\Coroutine;
+use Imi\Event\Event;
 
 /**
  * @Listener(eventName="IMI.MAIN_SERVER.WORKER.START",priority=PHP_INT_MIN)
@@ -25,5 +26,11 @@ class AfterWorkerStart implements IWorkerStartEventListener
 			Coroutine::resume($id);
 		}
 		unset($GLOBALS['WORKER_START_END_RESUME_COIDS']);
+
+		// 触发项目的workerstart事件
+		Event::trigger('IMI.MAIN_SERVER.WORKER.START.APP', [
+			'server'	=>	$e->server,
+			'workerID'	=>	$e->workerID,
+		], $e->getTarget(), WorkerStartEventParam::class);
 	}
 }
