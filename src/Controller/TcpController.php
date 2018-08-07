c3850d9002527f50ec2b68342d5d3b9d3f82397b
宇润 <admin@yurunsoft.com>
2018-08-07 16:53:22 +0800

    新增TcpController->encodeMessage()方法

--- src/Controller/TcpController.php
+++ src/Controller/TcpController.php
@@ -17,4 +17,15 @@ abstract class TcpController
 	 * @var \Imi\Server\TcpServer\Message\IReceiveData
 	 */
 	public $data;
+
+	/**
+	 * 编码消息，把数据编码为发送给客户端的格式
+	 *
+	 * @param mixed $data
+	 * @return mixed
+	 */
+	protected function encodeMessage($data)
+	{
+		return $this->server->getBean(\Imi\Server\DataParser\DataParser::class)->encode($data);
+	}
 }
