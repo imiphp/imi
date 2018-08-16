c0a3f4f0ca82bcf444cb23acae43393d279ec200
宇润 <admin@yurunsoft.com>
2018-08-16 10:40:12 +0800

    调整优化BeanFactory实例化类时参数处理逻辑

--- src/Bean/BeanFactory.php
+++ src/Bean/BeanFactory.php
@@ -121,11 +121,14 @@ TPL;
 			$tpl .= <<<TPL
 	public function {$method->name}({$paramsTpls['define']}){$methodReturnType}
 	{
-		\$__args__ = [{$paramsTpls['args']}];{$paramsTpls['args_variadic']}
+		\$__args__ = func_get_args();
+		{$paramsTpls['set_args']}
 		return \$this->beanProxy->call(
 			'{$method->name}',
 			function({$paramsTpls['define']}){
-				return parent::{$method->name}({$paramsTpls['call']});
+				\$__args__ = func_get_args();
+				{$paramsTpls['set_args']}
+				return parent::{$method->name}(...\$__args__);
 			},
 			\$__args__
 		);
@@ -147,10 +150,11 @@ TPL;
 			'args'			=>	[],
 			'define'		=>	[],
 			'call'			=>	[],
+			'set_args'		=>	'',
 		];
-		foreach($method->getParameters() as $param)
+		foreach($method->getParameters() as $i => $param)
 		{
-			// 数组参数，支持引用传参
+			// 数组参数，支持可变传参
 			if(!$param->isVariadic())
 			{
 				$result['args'][] = static::getMethodParamArgsTpl($param);
@@ -159,8 +163,13 @@ TPL;
 			$result['define'][] = static::getMethodParamDefineTpl($param);
 			// 调用传参
 			$result['call'][] = static::getMethodParamCallTpl($param);
+			// 引用传参
+			if($param->isPassedByReference())
+			{
+				$result['set_args'] .= '$__args__[' . $i . '] = &$' . $param->name . ';';
+			}
 		}
-		foreach($result as &$item)
+		foreach($result as $key => &$item)
 		{
 			if(is_array($item))
 			{
@@ -172,23 +181,6 @@ TPL;
 		{
 			$result['call'] = '...func_get_args()';
 		}
-		// 可变参数
-		if(isset($param) && $param->isVariadic())
-		{
-			$result['args_variadic'] = static::getMethodArgsVariadicTpl($param);
-		}
-		else
-		{
-			$result['args_variadic'] = '';
-		}
-		$result['args_variadic'] .= <<<STR
-
-		if(!isset(\$__args__[func_num_args() - 1]))
-		{
-			\$__allArgs__ = func_get_args();
-			\$__args__ = array_merge(\$__args__, array_splice(\$__allArgs__, count(\$__args__)));
-		}
-STR;
 		return $result;
 	}
 
@@ -249,22 +241,6 @@ STR;
 	private static function getMethodParamCallTpl(\ReflectionParameter $param)
 	{
 		return ($param->isVariadic() ? '...' : '') . '$' . $param->name;
-	}
-
-	/**
-	 * 获取方法可变参数模版
-	 * @param \ReflectionParameter $param
-	 * @return string
-	 */
-	private static function getMethodArgsVariadicTpl(\ReflectionParameter $param)
-	{
-		return <<<TPL
-
-		foreach(\${$param->name} as \$__item__)
-		{
-			\$__args__[] = \$__item__;
-		}
-TPL;
 	}
 
 	/**
