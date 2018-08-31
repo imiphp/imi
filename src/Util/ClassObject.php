94742554f4b6610ff327b192a171ba8e21fc1b4f
宇润 <admin@yurunsoft.com>
2018-08-31 14:55:25 +0800

    修复工具类BUG

--- src/Util/ClassObject.php
+++ src/Util/ClassObject.php
@@ -1,15 +1,19 @@
 <?php
 namespace Imi\Util;
 
+/**
+ * 类和对象相关工具类
+ */
 abstract class ClassObject
 {
 	/**
-	 * 是否是匿名类
+	 * 是否是匿名类对象
 	 * @param object $object
 	 * @return boolean
 	 */
 	public static function isAnymous($object)
 	{
-		return strpos(get_class($object), 'class@anonymous') >= 0;
+		$index = strpos(get_class($object), 'class@anonymous');
+		return false !== $index && $index >= 0;
 	}
 }
