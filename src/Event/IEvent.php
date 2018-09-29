194abefaa0756add8129d0dbcdc69d782ff33d24
宇润 <admin@yurunsoft.com>
2018-09-29 09:04:07 +0800

    新增 IEvent 接口

--- src/Event/IEvent.php
+++ src/Event/IEvent.php
@@ -1,20 +1,10 @@
 <?php
 namespace Imi\Event;
 
-use Imi\Util\Traits\TSingleton;
+use Imi\Event\EventParam;
 
-class Event implements IEvent
+interface IEvent
 {
-    use TEvent
-    {
-        on as public __on;
-        one as public __one;
-        off as public __off;
-        trigger as public __trigger;
-    }
-
-    use TSingleton;
-
     /**
      * 事件监听
      * @param string $name 事件名称
@@ -22,10 +12,7 @@ class Event implements IEvent
      * @param int $priority 优先级，越大越先执行
      * @return void
      */
-    public static function on($name, $callback, $priority = 0)
-    {
-        static::getInstance()->__on($name, $callback, $priority);
-    }
+    public static function on($name, $callback, $priority = 0);
 
     /**
      * 监听事件，仅触发一次
@@ -34,10 +21,7 @@ class Event implements IEvent
      * @param int $priority 优先级，越大越先执行
      * @return void
      */
-    public static function one($name, $callback, $priority = 0)
-    {
-        static::getInstance()->__one($name, $callback, $priority);
-    }
+    public static function one($name, $callback, $priority = 0);
 
     /**
      * 取消事件监听
@@ -45,10 +29,7 @@ class Event implements IEvent
      * @param mixed $callback 回调，支持回调函数、基于IEventListener的类名
      * @return void
      */
-    public static function off($name, $callback)
-    {
-        static::getInstance()->__off($name, $callback);
-    }
+    public static function off($name, $callback);
 
     /**
      * 触发事件
@@ -58,8 +39,5 @@ class Event implements IEvent
      * @param string $paramClass 参数类
      * @return void
      */
-    public static function trigger($name, $data = [], $target = null, $paramClass = EventParam::class)
-    {
-        static::getInstance()->__trigger($name, $data, $target, $paramClass);
-    }
+    public static function trigger($name, $data = [], $target = null, $paramClass = EventParam::class);
 }
