3d55b2bb7ddac4c62cf6ba62de0d59353e594f4e
宇润 <admin@yurunsoft.com>
2018-11-30 10:58:19 +0800

    新增 Callback 注解，其 class 可配合 Inject 注解，实现表示bean类方法回调

---
+++ src/Bean/Annotation/Callback.php
@@ -0,0 +1,37 @@
+<?php
+namespace Imi\Bean\Annotation;
+
+use Imi\Aop\Annotation\BaseInjectValue;
+
+/**
+ * 回调注解
+ * @Annotation
+ * @Target("ANNOTATION")
+ * @Parser("\Imi\Bean\Parser\NullParser")
+ */
+class Callback extends BaseInjectValue
+{
+    /**
+     * 类名，或者传入对象
+     *
+     * @var string|object
+     */
+    public $class;
+
+    /**
+     * 方法名
+     *
+     * @var string
+     */
+    public $method;
+
+    /**
+     * 获取注入值的真实值
+     *
+     * @return mixed
+     */
+    public function getRealValue()
+    {
+        return [$this->class, $this->method];
+    }
+}
