062e9c258d1ba7566ca114bccf68ec2d59cb00ba
宇润 <admin@yurunsoft.com>
2018-12-04 15:21:10 +0800

    新增 ConstValue 注解，从从常量中读取值注入注解属性

---
+++ src/Aop/Annotation/ConstValue.php
@@ -0,0 +1,49 @@
+<?php
+namespace Imi\Aop\Annotation;
+
+use Imi\Config;
+use Imi\Bean\Annotation\Parser;
+use Imi\Aop\Annotation\BaseInjectValue;
+
+/**
+ * 从常量中读取值
+ * 
+ * 支持在注解中为属性动态赋值
+ * 
+ * @Annotation
+ * @Target("ANNOTATION")
+ * @Parser("\Imi\Bean\Parser\NullParser")
+ */
+class ConstValue extends BaseInjectValue
+{
+    /**
+     * 只传一个参数时的参数名
+     * @var string
+     */
+    protected $defaultFieldName = 'name';
+
+    /**
+     * 配置名，支持@app、@currentServer等用法
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * 配置不存在时，返回的默认值
+     *
+     * @var mixed
+     */
+    public $default;
+
+    /**
+     * 获取注入值的真实值
+     *
+     * @return mixed
+     */
+    public function getRealValue()
+    {
+        return defined($this->name) ? constant($this->name) : $default;
+    }
+
+}
