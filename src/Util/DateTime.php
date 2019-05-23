74a04a9e3373d7d532d2b6d34e62ef254f428fa8
宇润 <admin@yurunsoft.com>
2019-05-23 11:36:21 +0800

    修复 Util\DateTime 问题

--- src/Util/DateTime.php
+++ src/Util/DateTime.php
@@ -1,17 +1,20 @@
 <?php
 namespace Imi\Util;
 
+/**
+ * 日期时间工具类
+ */
 abstract class DateTime
 {
     /**
-     * 将一个 \DateInterval，与当前时间进行计算，获取毫秒数
+     * 将一个 \DateInterval，与当前时间进行计算，获取秒数
      *
      * @param \DateInterval $dateInterval
      * @return int
      */
     public static function getSecondsByInterval(\DateInterval $dateInterval)
     {
-        $dateTime = new DateTime;
+        $dateTime = new \DateTime;
         $dateTime->add($dateInterval);
         return $dateTime->getTimestamp() - time();
     }
