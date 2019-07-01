48ac39013048059be47be5850cc890808a60a59a
Yurun <admin@yurunsoft.com>
2019-07-01 15:40:34 +0800

    新增 Redis 测试用例

--- tests/unit/BaseTest.php
+++ tests/unit/BaseTest.php
@@ -5,5 +5,26 @@ use PHPUnit\Framework\TestCase;
 
 abstract class BaseTest extends TestCase
 {
-    
+    protected function go($callable)
+    {
+        $throwable = null;
+        $end = false;
+        imigo(function() use($callable, &$throwable, &$end){
+            try {
+                $callable();
+            } catch(\Throwable $th) {
+                $throwable = $th;
+            }
+            $end = true;
+        });
+        while(!$end)
+        {
+            \Swoole\Event::dispatch();
+        }
+        if($throwable)
+        {
+            throw $throwable;
+        }
+    }
+
 }
--- tests/unit/Component/config/config.php
+++ tests/unit/Component/config/config.php
@@ -9,9 +9,47 @@ return [
         'Imi\Test\Component\Inject',
         'Imi\Test\Component\Event',
         'Imi\Test\Component\Enum',
+        'Imi\Test\Component\Redis',
     ],
     'beans'    =>    [
     ],
     'imi'   =>  'very six',
     'yurun' =>  '',
+    
+    // 连接池配置
+    'pools'    =>    [
+        'redis_test'    =>    [
+            'sync'    =>    [
+                'pool'    =>    [
+                    'class'        =>    \Imi\Redis\SyncRedisPool::class,
+                    'config'    =>    [
+                        'maxResources'    =>    10,
+                        'minResources'    =>    1,
+                    ],
+                ],
+                'resource'    =>    [
+                    'host'        => '127.0.0.1',
+                    'port'        =>    6379,
+                ]
+            ],
+            'async'    =>    [
+                'pool'    =>    [
+                    'class'        =>    \Imi\Redis\CoroutineRedisPool::class,
+                    'config'    =>    [
+                        'maxResources'    =>    10,
+                        'minResources'    =>    1,
+                    ],
+                ],
+                'resource'    =>    [
+                    'host'        => '127.0.0.1',
+                    'port'        =>    6379,
+                ]
+            ],
+        ],
+    ],
+    // redis 配置
+    'redis' =>  [
+        // 数默认连接池名
+        'defaultPool'   =>  'redis_test',
+    ],
 ];
