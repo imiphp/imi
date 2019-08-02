747a11c739be1f0370d3e42b77890f1df283081b
Yurun <admin@yurunsoft.com>
2019-08-02 13:44:22 +0800

    增加 Log 测试用例

---
+++ tests/unit/Component/Tests/LogTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace Imi\Test\Component\Tests;
+
+use Imi\Test\BaseTest;
+use Imi\Log\Log;
+use Imi\Log\LogLevel;
+
+/**
+ * @testdox Log
+ */
+class LogTest extends BaseTest
+{
+    public function testLog()
+    {
+        ob_start();
+        Log::alert('alert');
+        Log::critical('critical');
+        Log::debug('debug');
+        Log::emergency('emergency');
+        Log::error('error');
+        Log::info('info');
+        Log::notice('notice');
+        Log::warning('warning');
+        Log::log(LogLevel::INFO, 'info2');
+        ob_end_clean();
+        $this->assertTrue(true, 'Log fail');
+    }
+
+}
