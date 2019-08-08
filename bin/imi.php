86f33fb2514ebb389a4ea350caef8b9673585b72
Yurun <admin@yurunsoft.com>
2019-08-08 10:19:57 +0800

    支持使用 vendor/bin/imi 启动项目 会自动尝试加载 vendor/autoload.php，顺序如下： 工作目录 框架目录 没有则抛出错误

--- bin/imi.php
+++ bin/imi.php
@@ -1,33 +1,44 @@
 <?php
-if(!class_exists('Imi\App'))
-{
-    $fileName = dirname(__DIR__) . '/vendor/autoload.php';
-    if(!is_file($fileName))
-    {
-        echo 'No file vendor/autoload.php', PHP_EOL;
-        exit;
-    }
-    require $fileName;
-    unset($fileName);
-}
-
 use Imi\App;
 use Imi\Util\Args;
 use Imi\Util\File;
 
-Args::init(2);
-
-$namespace = Args::get('appNamespace');
-if(null === $namespace)
-{
-    $config = include File::path(dirname($_SERVER['SCRIPT_NAME'], 2), 'config/config.php');
-    if(!isset($config['namespace']))
+$imi = new class{
+    public function run()
     {
-        echo 'Has no namespace, please add arg: -appNamespace "Your App Namespace"', PHP_EOL;
-        exit;
-    }
-    $namespace = $config['namespace'];
-    unset($config);
-}
+        if(!class_exists('Imi\App'))
+        {
+            $fileName = $_SERVER['PWD'] . '/vendor/autoload.php';
+            if(!is_file($fileName))
+            {
+                $fileName = dirname(__DIR__) . '/vendor/autoload.php';
+            }
+            if(!is_file($fileName))
+            {
+                echo 'No file vendor/autoload.php', PHP_EOL;
+                exit;
+            }
+            $loader = require $fileName;
+            App::setLoader($loader);
+            unset($fileName, $loader);
+        }
 
-App::run($namespace);
+        Args::init(2);
+
+        $namespace = Args::get('appNamespace');
+        if(null === $namespace)
+        {
+            $config = include File::path(dirname($_SERVER['SCRIPT_NAME'], 2), 'config/config.php');
+            if(!isset($config['namespace']))
+            {
+                echo 'Has no namespace, please add arg: -appNamespace "Your App Namespace"', PHP_EOL;
+                exit;
+            }
+            $namespace = $config['namespace'];
+            unset($config);
+        }
+
+        App::run($namespace);
+    }
+};
+$imi->run();
