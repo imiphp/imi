26df6cf62ce3c678b3344397b10892df41884ab3
Yurun <admin@yurunsoft.com>
2019-08-14 15:48:57 +0800

    修复 WebSocket 热重启后无法握手问题

--- src/Server/ConnectContext/Listener/WorkerStart.php
+++ src/Server/ConnectContext/Listener/WorkerStart.php
@@ -1,13 +1,14 @@
 <?php
 namespace Imi\Server\ConnectContext\Listener;
 
+use Imi\Worker;
+use Imi\Util\Imi;
 use Imi\ServerManage;
+use Imi\Bean\BeanProxy;
 use Imi\RequestContext;
 use Imi\Bean\Annotation\Listener;
 use Imi\Server\Event\Param\WorkerStartEventParam;
 use Imi\Server\Event\Listener\IWorkerStartEventListener;
-use Imi\Bean\BeanProxy;
-use Imi\Util\Imi;
 
 /**
  * @Listener(eventName="IMI.MAIN_SERVER.WORKER.START")
@@ -21,7 +22,7 @@ class WorkerStart implements IWorkerStartEventListener
      */
     public function handle(WorkerStartEventParam $e)
     {
-        if(!$e->server->getSwooleServer()->taskworker)
+        if(!$e->server->getSwooleServer()->taskworker && 0 === Worker::getWorkerID())
         {
             RequestContext::create();
             foreach(ServerManage::getServers() as $server)
