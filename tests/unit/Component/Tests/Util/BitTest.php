a2e3f24d6569118399f163e6dd1cdc6fd36a8bf1
Yurun <admin@yurunsoft.com>
2019-08-16 14:25:36 +0800

    新增 Imi\Util\Bit 测试用例

---
+++ tests/unit/Component/Tests/Util/BitTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Imi\Test\Component\Tests\Util;
+
+use Imi\Test\BaseTest;
+use Imi\Util\Bit;
+
+/**
+ * @testdox Imi\Util\Bit
+ */
+class BitTest extends BaseTest
+{
+    /**
+     * @testdox has
+     *
+     * @return void
+     */
+    public function testHas()
+    {
+        $value = 1 + 4 + 256;
+        $this->assertTrue(Bit::has($value, 1));
+        $this->assertTrue(Bit::has($value, 4));
+        $this->assertTrue(Bit::has($value, 256));
+        $this->assertTrue(Bit::has($value, 5));
+        $this->assertFalse(Bit::has($value, 8));
+        $this->assertFalse(Bit::has($value, 16));
+    }
+
+}
