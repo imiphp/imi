f31a13dd5cd56f5137070c0f1085220583756dc3
Yurun <admin@yurunsoft.com>
2019-08-19 16:56:30 +0800

    修复测试用例问题

--- tests/unit/AppBaseMain.php
+++ tests/unit/AppBaseMain.php
@@ -2,6 +2,7 @@
 namespace Imi\Test;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Swoole\Coroutine;
 
 abstract class AppBaseMain extends \Imi\Main\AppBaseMain
 {
@@ -10,9 +11,12 @@ abstract class AppBaseMain extends \Imi\Main\AppBaseMain
         // 这里可以做一些初始化操作，如果需要的话
         
         AnnotationReader::addGlobalIgnoredName('testdox');
-        \swoole_async_set([
-            'max_thread_num'    =>  4,
-        ]);
+        if(Coroutine::getuid() < 0 && version_compare(SWOOLE_VERSION, '4.4.4', '>='))
+        {
+            \Swoole\Async::set([
+                'max_thread_num'    =>  4,
+            ]);
+        }
     }
 
 }
