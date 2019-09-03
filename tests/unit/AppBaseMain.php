bbf054dd4618c22ad38fa81835e0158493be73b8
Yurun <admin@yurunsoft.com>
2019-09-03 10:01:04 +0800

    调整测试配置

--- tests/unit/AppBaseMain.php
+++ tests/unit/AppBaseMain.php
@@ -11,7 +11,7 @@ abstract class AppBaseMain extends \Imi\Main\AppBaseMain
         // 这里可以做一些初始化操作，如果需要的话
         
         AnnotationReader::addGlobalIgnoredName('testdox');
-        if(Coroutine::getuid() < 0 && version_compare(SWOOLE_VERSION, '4.4.4', '>='))
+        if(Coroutine::getuid() < 0 && version_compare(SWOOLE_VERSION, '4.4.4', '='))
         {
             \swoole_async_set([
                 'max_thread_num'    =>  4,
