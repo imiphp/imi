bab409e1cfab4b029d21f48951c44e69c6aea461
Yurun <admin@yurunsoft.com>
2019-09-08 09:34:27 +0800

    优化测试用例

--- .travis/output-server-log.php
+++ .travis/output-server-log.php
@@ -6,6 +6,7 @@ $servers = [
     'RedisSessionServer',
     'TCPServer',
     'UDPServer',
+    'WebSocketServer',
 ];
 
 $date = date('Y-m-d');
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -34,7 +34,8 @@ function startServer()
         for($i = 0; $i < 60; ++$i)
         {
             sleep(1);
-            if('imi' === @file_get_contents('http://127.0.0.1:13001/'))
+            $context = stream_context_create(['http'=>['timeout'=>1]]);
+            if('imi' === @file_get_contents('http://127.0.0.1:13001/', false, $context))
             {
                 $serverStarted = true;
                 break;
@@ -49,7 +50,8 @@ function startServer()
         for($i = 0; $i < 60; ++$i)
         {
             sleep(1);
-            @file_get_contents('http://127.0.0.1:13002/');
+            $context = stream_context_create(['http'=>['timeout'=>1]]);
+            @file_get_contents('http://127.0.0.1:13002/', false, $context);
             if(isset($http_response_header[0]) && 'HTTP/1.1 400 Bad Request' === $http_response_header[0])
             {
                 $serverStarted = true;
