84c60172bc6134a57b6f7512de225e9093c93e31
Yurun <admin@yurunsoft.com>
2019-10-17 17:23:02 +0800

    优化测试用例

--- tests/unit/Component/Tests/RedisModelTest.php
+++ tests/unit/Component/Tests/RedisModelTest.php
@@ -118,7 +118,7 @@ class RedisModelTest extends BaseTest
         ]);
         $this->assertEquals($expected, $record->toArray());
 
-        sleep(2);
+        sleep(3);
         $record = TestRedisModel2::find([
             'id'    =>  1,
             'name'  =>  'a',
