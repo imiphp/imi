9d387227b2f0d05ace9d38dcdbff39ec25799c9b
Yurun <admin@yurunsoft.com>
2019-12-13 13:19:57 +0800

    server/reload 命令增加 -runtime 参数，可以更新运行时缓存后，再重新加载服务

--- doc/dev/server.md
+++ doc/dev/server.md
@@ -22,7 +22,7 @@ HttpDemo/bin/imi server/stop
 
 ## 重新加载服务
 
-可以让项目文件更改生效
+重启 Worker 进程，不会导致连接断开，可以让项目文件更改生效
 
 无参数
 
@@ -30,4 +30,10 @@ HttpDemo/bin/imi server/stop
 
 ```
 HttpDemo/bin/imi server/reload
+```
+
+更新运行时缓存后，再重新加载服务
+
+```
+HttpDemo/bin/imi server/reload -runtime
 ```
--- src/Tool/Tools/Server/Server.php
+++ src/Tool/Tools/Server/Server.php
@@ -5,12 +5,12 @@ use Imi\App;
 use Imi\Util\Imi;
 use Imi\ServerManage;
 use Imi\Tool\ArgType;
-use Imi\RequestContext;
 use Imi\Pool\PoolManager;
 use Imi\Cache\CacheManager;
 use Imi\Tool\Annotation\Arg;
 use Imi\Tool\Annotation\Tool;
 use Imi\Tool\Annotation\Operation;
+use Imi\Tool\Tools\Imi\Imi as ToolImi;
 
 /**
  * @Tool("server")
@@ -56,15 +56,28 @@ class Server
     }
 
     /**
-     * 重新加载服务，可以让项目文件更改生效
+     * 重新加载服务
+     * 
+     * 重启 Worker 进程，不会导致连接断开，可以让项目文件更改生效
      * 
      * @Operation("reload")
+     * @Arg(name="runtime", type=ArgType::BOOL, required=false, default=false, comments="是否更新运行时缓存")
      * 
      * @return void
      */
-    public function reload()
+    public function reload($runtime)
     {
+        if($runtime)
+        {
+            $imi = new ToolImi;
+            echo 'Building runtime...', PHP_EOL;
+            $time = microtime(true);
+            $imi->buildRuntime('', null, false);
+            $useTime = microtime(true) - $time;
+            echo 'Runtime build complete! ', $useTime, 's', PHP_EOL;
+        }
         $result = Imi::reloadServer();
         echo $result['cmd'], PHP_EOL;
     }
+
 }
