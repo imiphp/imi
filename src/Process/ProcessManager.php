c608257c3845b718dd69e76ce381f66b76419f53
Yurun <admin@yurunsoft.com>
2020-07-01 15:00:45 +0800

    修复在子进程中，使用 exit 无法正确返回状态码的问题

--- src/Process/ProcessManager.php
+++ src/Process/ProcessManager.php
@@ -4,14 +4,15 @@ namespace Imi\Process;
 use Imi\App;
 use Imi\Util\Imi;
 use Imi\Util\File;
+use Swoole\Process;
 use Imi\Event\Event;
 use Imi\ServerManage;
 use Imi\Bean\BeanFactory;
+use Swoole\ExitException;
 use Imi\Util\Process\ProcessType;
 use Imi\Process\Parser\ProcessParser;
 use Imi\Util\Process\ProcessAppContexts;
 use Imi\Process\Exception\ProcessAlreadyRunException;
-use Swoole\Process;
 
 /**
  * 进程管理类
@@ -76,7 +77,7 @@ abstract class ProcessManager
      * @param string|null $alias
      * @return callable
      */
-    private static function getProcessCallable($args, $name, $processOption, ?string $alias = null)
+    public static function getProcessCallable($args, $name, $processOption, ?string $alias = null)
     {
         return function(\Swoole\Process $swooleProcess) use($args, $name, $processOption, $alias){
             App::set(ProcessAppContexts::PROCESS_TYPE, ProcessType::PROCESS, true);
@@ -94,32 +95,37 @@ abstract class ProcessManager
             \Swoole\Runtime::enableCoroutine(true);
             // 随机数播种
             mt_srand();
-            $callable = function() use($swooleProcess, $args, $name, $processOption){
-                if($processOption['Process']->unique && !static::lockProcess($name))
-                {
-                    throw new \RuntimeException('lock process lock file error');
+            $exitCode = 0;
+            $callable = function() use($swooleProcess, $args, $name, $processOption, &$exitCode){
+                try {
+                    if($processOption['Process']->unique && !static::lockProcess($name))
+                    {
+                        throw new \RuntimeException('lock process lock file error');
+                    }
+                    \Imi\Util\Process::clearNotInheritableSignalListener();
+                    // 加载服务器注解
+                    \Imi\Bean\Annotation::getInstance()->init(\Imi\Main\Helper::getAppMains());
+                    App::initWorker();
+                    // 进程开始事件
+                    Event::trigger('IMI.PROCESS.BEGIN', [
+                        'name'      => $name,
+                        'process'   => $swooleProcess,
+                    ]);
+                    // 执行任务
+                    $processInstance = BeanFactory::newInstance($processOption['className'], $args);
+                    $processInstance->run($swooleProcess);
+                    if($processOption['Process']->unique)
+                    {
+                        static::unlockProcess($name);
+                    }
+                    // 进程结束事件
+                    Event::trigger('IMI.PROCESS.END', [
+                        'name'      => $name,
+                        'process'   => $swooleProcess,
+                    ]);
+                } catch(ExitException $e) {
+                    $exitCode = $e->getStatus();
                 }
-                \Imi\Util\Process::clearNotInheritableSignalListener();
-                // 加载服务器注解
-                \Imi\Bean\Annotation::getInstance()->init(\Imi\Main\Helper::getAppMains());
-                App::initWorker();
-                // 进程开始事件
-                Event::trigger('IMI.PROCESS.BEGIN', [
-                    'name'      => $name,
-                    'process'   => $swooleProcess,
-                ]);
-                // 执行任务
-                $processInstance = BeanFactory::newInstance($processOption['className'], $args);
-                $processInstance->run($swooleProcess);
-                if($processOption['Process']->unique)
-                {
-                    static::unlockProcess($name);
-                }
-                // 进程结束事件
-                Event::trigger('IMI.PROCESS.END', [
-                    'name'      => $name,
-                    'process'   => $swooleProcess,
-                ]);
             };
             if($processOption['Process']->co)
             {
@@ -130,6 +136,10 @@ abstract class ProcessManager
                 $callable();
             }
             \Swoole\Event::wait();
+            if(0 != $exitCode)
+            {
+                exit($exitCode);
+            }
         };
     }
 
