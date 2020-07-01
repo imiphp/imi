460ca67fef849e666d988f7b1b2839a63c3c6c65
Yurun <admin@yurunsoft.com>
2020-07-01 15:51:00 +0800

    修复测试

--- tests/unit/HttpServer/Tests/ProcessTest.php
+++ tests/unit/HttpServer/Tests/ProcessTest.php
@@ -15,7 +15,7 @@ class ProcessTest extends BaseTest
      */
     public function testPoolCleanAllow()
     {
-        $dir = dirname(__DIR__) . '/bin/imi process/run -name PoolTest1';
+        $dir = dirname(__DIR__) . '/bin/imi process/start -name PoolTest1';
         $result = `{$dir}`;
         $list = explode(PHP_EOL, $result);
         end($list);
@@ -37,7 +37,6 @@ class ProcessTest extends BaseTest
         $result = `{$dir}`;
         $list = explode(PHP_EOL, $result);
         end($list);
-        prev($list);
         $this->assertEquals(json_encode([
             'maindb'    =>  0,
             'redis'     =>  1,
