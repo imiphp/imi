0ee14d3e3838be0c46b9de3affa1fe1da16bd2d9
Yurun <admin@yurunsoft.com>
2020-11-01 13:33:32 +0800

    新增正则验证注解

---
+++ src/Validate/Annotation/Regex.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Imi\Validate\Annotation;
+
+use Imi\Bean\Annotation;
+use Imi\Bean\Annotation\Parser;
+
+/**
+ * 正则验证
+ *
+ * @Annotation
+ * @Target({"CLASS", "METHOD", "PROPERTY"})
+ * @Parser("\Imi\Validate\Annotation\Parser\ValidateConditionParser")
+ */
+class Regex extends Condition
+{
+    /**
+     * 正则表达式文本.
+     *
+     * @var string
+     */
+    public $pattern;
+
+    /**
+     * 验证回调.
+     *
+     * @var callable
+     */
+    public $callable = '\Imi\Validate\ValidatorHelper::regex';
+
+    /**
+     * 参数名数组.
+     *
+     * @var array
+     */
+    public $args = [
+        '{:value}',
+        '{pattern}',
+    ];
+}
