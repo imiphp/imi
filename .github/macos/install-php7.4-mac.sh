65978c64146675c388716104aff485fb93ddc679
Yurun <admin@yurunsoft.com>
2020-11-30 20:25:19 +0800

    Macos test (#73)
    * 增加 macos 测试
    
    * 修复
    
    * 修复
    
    * 修复
    
    * 修复
    
    * 修复
    
    * 增加服务启动失败日志输出
    
    * 修复
    
    * 修复
    
    * 修复
    
    * 修复
    
    * 修复

---
+++ .github/macos/install-php7.4-mac.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PHP_VERSION="7.4"
+brew install php@$PHP_VERSION;
+brew link --force --overwrite php@7.4
+
+php -v
+php -m
+php-config
+
+curl -o composer.phar https://getcomposer.org/composer-stable.phar && chmod +x composer.phar && sudo mv -f composer.phar /usr/local/bin/composer && composer -V;
+
+# redis
+echo "no" | pecl install -f redis;
