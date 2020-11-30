169048e439b91170e9bb250642ee0e1a085439f4
Yurun <admin@yurunsoft.com>
2020-11-30 17:12:43 +0800

    增加 macos 测试

---
+++ .github/macos/install-swoole4.5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+swooleVersion="4.5.9"
+
+wget -O swoole.tar.gz https://github.com/swoole/swoole-src/archive/v$swooleVersion.tar.gz
+
+swooleDir="swoole-src-${swooleVersion}"
+
+tar -xzf swoole.tar.gz
+rm swoole.tar.gz
+
+cd $swooleDir
+
+phpize && ./configure && make -j && make install
+
+PHP_INI_FILE=$(php -r "echo php_ini_loaded_file();")
+if [[ $PHP_INI_FILE == "" ]]; then
+    PHP_INI_FILE="/usr/local/etc/php/$(php -r "echo (double)PHP_VERSION;")/php.ini";
+fi
+echo "extension = swoole.so" >> $PHP_INI_FILE
+
+cd ../
+
+rm -rf $swooleDir
+
+php --ri swoole
