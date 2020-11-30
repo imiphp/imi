b0a126ba0ac37ad183ee8412749e9925403745df
Yurun <admin@yurunsoft.com>
2020-11-30 14:15:05 +0800

    修复 Mysqli 驱动的编码不生效问题 #71

--- src/Db/Drivers/Mysqli/Driver.php
+++ src/Db/Drivers/Mysqli/Driver.php
@@ -117,7 +117,8 @@ class Driver extends Base implements IDb
     public function open()
     {
         $option = $this->option;
-        $this->instance = new \mysqli($option['host'] ?? '127.0.0.1', $option['username'], $option['password'], $option['database'], $option['port'] ?? 3306);
+        $this->instance = $instance = new \mysqli($option['host'] ?? '127.0.0.1', $option['username'], $option['password'], $option['database'], $option['port'] ?? 3306);
+        $instance->set_charset($option['charset']);
 
         return true;
     }
