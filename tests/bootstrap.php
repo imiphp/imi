ed4c7562d362c9b486118332200132ca22f66893
Yurun <admin@yurunsoft.com>
2021-01-13 09:59:20 +0800

    修复测试

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,16 @@ require __DIR__ . '/vendor/autoload.php';
 use Imi\App;
 
 App::setLoader($loader);
-\Swoole\Runtime::enableCoroutine();
+if (defined('SWOOLE_HOOK_NATIVE_CURL'))
+{
+    // 暂时移除 PHP 8 中有 bug 的 hook native curl
+    $flags = \SWOOLE_HOOK_ALL ^ \SWOOLE_HOOK_NATIVE_CURL;
+}
+else
+{
+    $flags = \SWOOLE_HOOK_ALL;
+}
+\Swoole\Runtime::enableCoroutine($flags);
 
 register_shutdown_function(function () {
     App::getBean('Logger')->save();
