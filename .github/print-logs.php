e956bf67a014044496e01693d9a4441a63449d50
Yurun <admin@yurunsoft.com>
2021-01-19 14:29:41 +0800

    更新

--- .github/print-logs.php
+++ .github/print-logs.php
@@ -11,8 +11,18 @@ else
     echo 'Not found!', \PHP_EOL;
 }
 
+echo '[Swoole Component]', \PHP_EOL;
+$fileName = dirname(__DIR__) . '/tests/unit/Component/logs/' . date('Y-m-d') . '.log';
+if (is_file($fileName))
+{
+    echo file_get_contents($fileName), \PHP_EOL;
+}
+else
+{
+    echo 'Not found!', \PHP_EOL;
+}
+
 foreach ([
-    'Component',
     'HttpServer',
     'RedisSessionServer',
     'WebSocketServer',
@@ -20,8 +30,24 @@ foreach ([
     'UDPServer',
 ] as $name)
 {
-    echo '[', $name, ']', \PHP_EOL;
-    $fileName = dirname(__DIR__) . '/src/Components/Swoole/tests/unit/' . $name . '/logs/' . date('Y-m-d') . '.log';
+    echo '[Swoole ', $name, ']', \PHP_EOL;
+    $fileName = dirname(__DIR__) . '/src/Components/Swoole/tests/unit/' . $name . '/logs/cli.log';
+    if (is_file($fileName))
+    {
+        echo file_get_contents($fileName), \PHP_EOL;
+    }
+    else
+    {
+        echo 'Not found!', \PHP_EOL;
+    }
+}
+
+foreach ([
+    'HttpServer',
+] as $name)
+{
+    echo '[Workerman ', $name, ']', \PHP_EOL;
+    $fileName = dirname(__DIR__) . '/src/Components/Workerman/tests/unit/' . $name . '/logs/cli.log';
     if (is_file($fileName))
     {
         echo file_get_contents($fileName), \PHP_EOL;
