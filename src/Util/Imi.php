643c322e6ba7acea09393e4b254a6492d21acd06
Yurun <admin@yurunsoft.com>
2021-01-28 10:21:07 +0800

    修复 stopServer、reloadServer

--- src/Util/Imi.php
+++ src/Util/Imi.php
@@ -539,16 +539,10 @@ class Imi
         {
             throw new \RuntimeException(sprintf('Pid file %s is not exists', $fileName));
         }
-        $return = [];
         $pid = json_decode(file_get_contents($fileName), true);
         if ($pid > 0)
         {
             Process::kill($pid['masterPID']);
-            $return['cmd'] = $cmd;
-            $result = `{$cmd}`;
-            $return['result'] = $result;
-
-            return $return;
         }
         else
         {
@@ -568,16 +562,10 @@ class Imi
         {
             throw new \RuntimeException(sprintf('Pid file %s is not exists', $fileName));
         }
-        $return = [];
         $pid = json_decode(file_get_contents($fileName), true);
         if ($pid > 0)
         {
             Process::kill($pid['masterPID'], \SIGUSR1);
-            $return['cmd'] = $cmd;
-            $result = `{$cmd}`;
-            $return['result'] = $result;
-
-            return $return;
         }
         else
         {
