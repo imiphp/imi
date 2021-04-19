4cfac28b5c286746ab47bc8cf7ed9998a04cbf7c
Yurun <admin@yurunsoft.com>
2021-04-19 15:27:53 +0800

    新增 ClassObject::convertArrayToKV()、convertKVToArray()、newInstance() 方法
    # Conflicts:
    #	tests/unit/Component/Tests/Util/ClassObjectTest.php

--- tests/unit/Component/Util/ClassObject/Test.php
+++ tests/unit/Component/Util/ClassObject/Test.php
@@ -7,6 +7,33 @@ namespace Imi\Test\Component\Util\ClassObject;
 class Test
 {
     /**
+     * @var mixed
+     */
+    public $a;
+
+    /**
+     * @var mixed
+     */
+    public $b;
+
+    /**
+     * @var string
+     */
+    public $c;
+
+    /**
+     * @param mixed  $a
+     * @param mixed  $b
+     * @param string $c
+     */
+    public function __construct($a, $b, $c = 'imi.com')
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+    }
+
+    /**
      * @param mixed  $a
      * @param mixed  $b
      * @param string $c
