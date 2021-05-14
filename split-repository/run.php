5e148dec5cfb50578c66246404c67692442edbaf
Yurun <admin@yurunsoft.com>
2021-05-14 17:29:28 +0800

    修复
    test
    
    优化代码

--- split-repository/run.php
+++ split-repository/run.php
@@ -19,7 +19,8 @@ function execCMD(string $cmd, string $description = '', ?array &$result = null):
     echo implode(\PHP_EOL, $result), \PHP_EOL;
     if (0 !== $resultCode)
     {
-        throw new \RuntimeException(sprintf('cmd status code is %s', $resultCode));
+        echo sprintf('cmd status code is %s', $resultCode), \PHP_EOL;
+        exit($resultCode);
     }
     echo '--end--', \PHP_EOL;
 }
@@ -224,7 +225,7 @@ foreach ($storeRepoMap as $name => $urls)
     $url = $urls[0];
     chdir(__DIR__);
     $repoName = basename($url, '.git');
-    $repoPath = '/tmp/' . $repoName . '/';
+    $repoPath = __DIR__ . $repoName . '/';
     if (is_dir($repoPath))
     {
         chdir($repoPath);
@@ -232,7 +233,7 @@ foreach ($storeRepoMap as $name => $urls)
     }
     else
     {
-        chdir('/tmp');
+        chdir(__DIR__);
         execCMD('git clone ' . $url, '克隆' . $url);
         chdir($repoPath);
     }
@@ -270,6 +271,7 @@ foreach ($storeRepoMap as $name => $urls)
     {
         chdir($mainRepoPath);
         execCMD('git --no-pager show ' . $commit . ' --stat', '提交记录', $result);
+        parseShowData($result, $author, $date, $message);
         $needCommit = false;
         foreach ($result as $row)
         {
@@ -307,7 +309,6 @@ foreach ($storeRepoMap as $name => $urls)
                     $originFileName = $mainRepoPath . $to;
                     if (is_file($originFileName))
                     {
-                        chdir($mainRepoPath);
                         $dir = dirname($repoFilePath);
                         if (!is_dir($dir))
                         {
@@ -331,7 +332,6 @@ foreach ($storeRepoMap as $name => $urls)
                     $originFileName = $mainRepoPath . $fileName;
                     if (is_file($originFileName))
                     {
-                        chdir($mainRepoPath);
                         $dir = dirname($repoFilePath);
                         if (!is_dir($dir))
                         {
@@ -359,8 +359,6 @@ foreach ($storeRepoMap as $name => $urls)
             continue;
         }
         chdir($mainRepoPath);
-        execCMD('git show ' . $commit, '', $showData);
-        parseShowData($showData, $author, $date, $message);
         $authorName = shell_exec('git show ' . $commit . ' -s --format=%cn');
         $authorEmail = shell_exec('git show ' . $commit . ' -s --format=%ce');
 
@@ -369,7 +367,7 @@ foreach ($storeRepoMap as $name => $urls)
         {
             execCMD('git branch -M ' . $branch, '');
         }
-        execCMD('git status -s', '', $result);
+        execCMD('git status -s -u no', '', $result);
         if ($result)
         {
             execCMD('git config user.name "' . $authorName . '" && git config user.email "' . $authorEmail . '" && git commit --author "' . $author . '" --date "' . $date . '" -am \'' . $message . '\'', 'git commit');
