7e639d56cd6f9ce1d2c862448898c69976cf5b17
zxin <auooru@outlook.com>
2021-05-17 21:21:11 -0500

    Github Actions 优化 (#90)
    * update phpcs test
    
    * update phpstan test
    
    * 暂时禁用 ci
    
    * update phpstan.yml
    
    * update phpstan.yml
    
    * update phpstan.yml
    
    * fix phpcs.yml
    
    * update workflow
    
    * remove 4.5 jit test
    
    * update mac test
    
    * update workflows
    
    * update mac test
    
    * update workflows
    
    * update dev/Plugin.php
    
    * update workflow composer option
    
    * fix phpstan.yml
    
    * update workflows
    
    * update phpstan.yml
    
    * update ci.yml
    
    * update rename
    
    * 修复问题

---
+++ .github/prepare-kafka.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "init kafka"
+(docker exec kafka1 /opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --create --partitions 3 --replication-factor 1 --topic queue-imi-1 \
+&& docker exec kafka1 /opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --create --partitions 3 --replication-factor 1 --topic QueueTest1 \
+&& docker exec kafka1 /opt/kafka/bin/kafka-topics.sh --zookeeper zookeeper:2181 --create --partitions 3 --replication-factor 1 --topic imi-kafka-queue-test) || echo "no kafka"
