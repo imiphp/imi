812f60896e84c15699086fa3beac4b6220fa88dc
Yurun <admin@yurunsoft.com>
2021-05-31 14:03:06 +0800

    修复关联模型问题

--- src/Model/Meta.php
+++ src/Model/Meta.php
@@ -175,8 +175,15 @@ class Meta
             }
             $fields[$name] = $column;
         }
+        $this->dbFields = $dbFields;
         $this->fields = $fields;
-        $this->fieldNames = $fieldNames = array_keys($fields);
+        $fieldNames = array_keys($fields);
+        $this->relation = ModelRelationManager::hasRelation($modelClass);
+        if ($this->relation)
+        {
+            $fieldNames = array_merge($fieldNames, ModelRelationManager::getRelationFieldNames($modelClass));
+        }
+        $this->fieldNames = $fieldNames;
         $this->camel = $camel = $entity->camel ?? false;
         $serializableFieldNames = [];
         foreach ($fieldNames as $fieldName)
@@ -191,7 +198,6 @@ class Meta
             }
         }
         $this->serializableFieldNames = $serializableFieldNames;
-        $this->dbFields = $dbFields;
         foreach ($fields as $field => $column)
         {
             if ($column->isAutoIncrement)
@@ -205,11 +211,6 @@ class Meta
         $this->extractPropertys = ModelManager::getExtractPropertys($modelClass);
         $this->propertyJsonNotNullMap = AnnotationManager::getPropertiesAnnotations($modelClass, JsonNotNull::class);
         $this->sqlColumns = AnnotationManager::getPropertiesAnnotations($modelClass, Sql::class);
-        $this->relation = ModelRelationManager::hasRelation($modelClass);
-        if ($this->relation)
-        {
-            $this->fieldNames = array_merge($this->fieldNames, ModelRelationManager::getRelationFieldNames($modelClass));
-        }
     }
 
     /**
