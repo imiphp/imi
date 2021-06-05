47f74e41ddfc0974c89e642edee0ab0bcb85b6d6
Yurun <admin@yurunsoft.com>
2021-06-05 18:55:02 +0800

    修正代码格式

--- src/Log/ErrorLog.php
+++ src/Log/ErrorLog.php
@@ -25,14 +25,14 @@ class ErrorLog
      *
      * @var int
      */
-    protected $catchLevel = E_ALL | E_STRICT;
+    protected $catchLevel = \E_ALL | \E_STRICT;
 
     /**
      * 抛出异常的错误级别.
      *
      * @var int
      */
-    protected $exceptionLevel = E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_WARNING | E_CORE_WARNING | E_COMPILE_WARNING | E_USER_WARNING;
+    protected $exceptionLevel = \E_ERROR | \E_PARSE | \E_CORE_ERROR | \E_COMPILE_ERROR | \E_USER_ERROR | \E_RECOVERABLE_ERROR | \E_WARNING | \E_CORE_WARNING | \E_COMPILE_WARNING | \E_USER_WARNING;
 
     /**
      * 回溯堆栈帧的数量限制.
