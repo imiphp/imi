cd945879696210b6bc1d7a4f4cf204d8350aa235
Yurun <admin@yurunsoft.com>
2021-07-05 15:51:07 +0800

    Workerman 根据环境自动启用 reusePort

--- src/Components/workerman/src/Server/WorkermanServerWorker.php
+++ src/Components/workerman/src/Server/WorkermanServerWorker.php
@@ -8,6 +8,25 @@ use Workerman\Worker;
 
 class WorkermanServerWorker extends Worker
 {
+    /**
+     * Construct.
+     *
+     * @param string $socket_name
+     */
+    public function __construct($socket_name = '', array $context_option = [])
+    {
+        parent::__construct($socket_name, $context_option);
+
+        if (static::$_OS === \OS_TYPE_LINUX  // if linux
+            && \version_compare(\PHP_VERSION,'7.0.0', 'ge') // if php >= 7.0.0
+            && \version_compare(php_uname('r'), '3.9', 'ge') // if kernel >=3.9
+            && \strtolower(\php_uname('s')) !== 'darwin' // if not Mac OS
+            && strpos($socket_name,'unix') !== 0) { // if not unix socket
+
+            $this->reusePort = true;
+        }
+    }
+
     public static function getMasterPid(): int
     {
         return static::$_masterPid;
