9d3870291566dcd2e2a2c5dde209c5db00304cb9
Yurun <admin@yurunsoft.com>
2021-07-28 09:22:14 +0800

    修复在未安装 git 的环境中的报错问题

--- src/App.php
+++ src/App.php
@@ -237,7 +237,7 @@ class App
             }
         }
         // git
-        if (false !== strpos(shell_exec('git --version') ?? '', 'git version') && preg_match('/\*([^\r\n]+)/', shell_exec('git branch') ?? '', $matches) > 0)
+        if (preg_match('/\*([^\r\n]+)/', shell_exec('which git && git branch') ?? '', $matches) > 0)
         {
             return static::$imiVersion = trim($matches[1]);
         }
