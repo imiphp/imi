4dc72583b97cc5fb8c58196370fdd5478b9a2dbb
Yurun <admin@yurunsoft.com>
2021-08-23 15:15:16 +0800

    修复

--- src/Components/swoole/src/Pool/BaseAsyncPool.php
+++ src/Components/swoole/src/Pool/BaseAsyncPool.php
@@ -60,12 +60,6 @@ abstract class BaseAsyncPool extends BasePool
             // 填充最少资源数
             $this->fillMinResources();
         }
-        else
-        {
-            go(function () {
-                $this->fillMinResources();
-            });
-        }
         // 定时资源回收
         $this->stopAutoGC();
         $this->startAutoGC();
