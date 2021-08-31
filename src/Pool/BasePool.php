29e740c56774a92fff0378c6730476f3ffc53e46
Yurun <admin@yurunsoft.com>
2021-08-31 14:44:37 +0800

    修复连接池资源回收时间精度问题

--- src/Pool/BasePool.php
+++ src/Pool/BasePool.php
@@ -185,29 +185,30 @@ abstract class BasePool implements IPool
      */
     public function gc()
     {
-        $hasGC = false;
-        $config = $this->config;
-        $maxActiveTime = $config->getMaxActiveTime();
-        $maxUsedTime = $config->getMaxUsedTime();
         $pool = &$this->pool;
         if ($pool)
         {
+            $hasGC = false;
+            $config = $this->config;
+            $maxActiveTime = $config->getMaxActiveTime();
+            $maxUsedTime = $config->getMaxUsedTime();
+            $time = microtime(true);
             foreach ($pool as $key => $item)
             {
                 if (
-                (null !== $maxActiveTime && $item->isFree() && time() - $item->getCreateTime() >= $maxActiveTime) // 最大存活时间
-                || (null !== $maxUsedTime && $item->getLastReleaseTime() < $item->getLastUseTime() && time() - $item->getLastUseTime() >= $maxUsedTime) // 每次获取资源最长使用时间
+                (null !== $maxActiveTime && $item->isFree() && $time - $item->getCreateTime() >= $maxActiveTime) // 最大存活时间
+                || (null !== $maxUsedTime && $item->getLastReleaseTime() < $item->getLastUseTime() && $time - $item->getLastUseTime() >= $maxUsedTime) // 每次获取资源最长使用时间
                 ) {
                     $item->getResource()->close();
                     unset($pool[$key]);
                     $hasGC = true;
                 }
             }
-        }
-        if ($hasGC)
-        {
-            $this->fillMinResources();
-            $this->buildQueue();
+            if ($hasGC)
+            {
+                $this->fillMinResources();
+                $this->buildQueue();
+            }
         }
     }
 
