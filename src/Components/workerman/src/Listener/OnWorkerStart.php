58aa2993f2d10b666835a2523fe1fd52b125fc44
Yurun <admin@yurunsoft.com>
2021-09-12 13:11:38 +0800

    优化当 Workerman worker 启动时，清理父进程带来的数据库连接

---
+++ src/Components/workerman/src/Listener/OnWorkerStart.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Workerman\Listener;
+
+use Imi\App;
+use Imi\Bean\Annotation\Listener;
+use Imi\Config;
+use Imi\Event\EventParam;
+use Imi\Event\IEventListener;
+
+/**
+ * @Listener("IMI.WORKERMAN.SERVER.WORKER_START")
+ */
+class OnWorkerStart implements IEventListener
+{
+    /**
+     * 事件处理方法.
+     */
+    public function handle(EventParam $e): void
+    {
+        foreach (Config::get('@app.db.connections') as $name => $_)
+        {
+            App::set('__db.' . $name, null);
+        }
+        foreach (Config::get('@app.redis.connections') as $name => $_)
+        {
+            App::set('__redis.' . $name, null);
+        }
+    }
+}
