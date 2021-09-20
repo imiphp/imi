ee932fca0519da61db42dca5582f41bf989ca090
zxin <auooru@outlook.com>
2021-09-20 18:20:13 +0800

    重构 mac 测试 (#153)
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 测试 macos
    
    * 改进 macos 测试
    
    * 改进 macos 测试

--- .github/macos/install-php.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -o composer.phar https://getcomposer.org/composer-stable.phar && chmod +x composer.phar && sudo mv -f composer.phar /usr/local/bin/composer && composer -V;
-
-# redis
-echo "no" | pecl install -f redis;
