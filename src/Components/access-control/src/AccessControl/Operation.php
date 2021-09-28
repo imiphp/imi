5186d08d8829efd83bf251668a1fafb4becf92fe
Mjc960512 <1361810132@qq.com>
2021-09-28 14:55:37 +0800

    完善权限控制组件权限操作代码，完善权限控制文档

--- src/Components/access-control/src/AccessControl/Operation.php
+++ src/Components/access-control/src/AccessControl/Operation.php
@@ -15,4 +15,36 @@ abstract class Operation
     {
         return App::getBean('ACOperationService')->create($name, $code, $parentId, $index, $description);
     }
+
+    /**
+     * 修改操作权限
+     */
+    public static function update(int $id, string $name, ?string $code, int $parentId = 0, int $index = 0, string $description = ''): bool
+    {
+        return App::getBean('ACOperationService')->update($id, $name, $code, $parentId, $index, $description);
+    }
+
+    /**
+     * 删除操作权限
+     */
+    public static function delete(int $id): bool
+    {
+        return App::getBean('ACOperationService')->delete($id);
+    }
+
+    /**
+     * 查询列表
+     */
+    public static function selectList(): array
+    {
+        return App::getBean('ACOperationService')->selectList();
+    }
+
+    /**
+     * 转为树形
+     */
+    public function listToTree(array $list): array
+    {
+        return App::getBean('ACOperationService')->listToTree($list);
+    }
 }
