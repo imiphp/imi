66878f8c894854c3f9fdaac48a9f4c1d727e7555
auooru <auooru@outlook.com>
2021-10-04 01:39:00 +0800

    优化并发安装

--- dev/Plugin.php
+++ dev/Plugin.php
@@ -6,11 +6,16 @@ namespace Imi\Dev;
 
 use FilesystemIterator;
 use Imi\Cli\ImiCommand;
+use Swoole\Coroutine;
+use function count;
+use function imiGetEnv;
 use function implode;
 use function method_exists;
 use Swoole\Event;
 use Swoole\Runtime;
 use Symfony\Component\Process\Process;
+use function realpath;
+use function usleep;
 
 class Plugin
 {
@@ -18,11 +23,11 @@ class Plugin
     {
         $componentsDir = \dirname(__DIR__) . '/src/Components';
         $output = ImiCommand::getOutput();
-        $isSwoole = \extension_loaded('swoole');
-        if ($isSwoole)
-        {
-            Runtime::enableCoroutine();
-        }
+        $maxCount = 4;
+        /** @var Process[] $readyProcesses */
+        $readyProcesses = [];
+        /** @var Process[] $activeProcesses */
+        $activeProcesses = [];
         foreach (new FilesystemIterator($componentsDir, FilesystemIterator::SKIP_DOTS) as $dir)
         {
             if (!$dir->isDir())
@@ -30,49 +35,66 @@ class Plugin
                 continue;
             }
             $fileName = $dir->getPathname() . '/composer.json';
-            if (is_file($fileName))
+            if (!is_file($fileName))
             {
-                $function = function () use ($output, $dir) {
-                    $output->writeln("[Update <info>{$dir->getBasename()}</info>]");
-                    $cmd = [
-                        \PHP_BINARY,
-                        realpath($_SERVER['SCRIPT_FILENAME']),
-                        'update',
-                        '--no-interaction',
-                        '--prefer-dist',
-                        '--no-progress',
-                    ];
-                    // 兼容 symfony process < 3.3
-                    if (method_exists(Process::class, 'fromShellCommandline'))
-                    {
-                        $p = new Process($cmd, $dir->getPathname(), null, null, 0);
-                    }
-                    else
-                    {
-                        $p = new Process([], $dir->getPathname(), null, null, 0);
-                        /* @phpstan-ignore-next-line */
-                        $p->setCommandLine(implode(' ', $cmd));
-                    }
-                    $p->run(function ($type, $buffer) {
-                        echo $buffer;
-                    });
+                continue;
+            }
+            $output->writeln("[Update <info>{$dir->getBasename()}</info>]");
+            $process = self::createProcess($dir);
+            $readyProcesses[$dir->getBasename()] = $process;
+        }
 
-                    $result = $p->isSuccessful() ? '<info>success</info>' : "<error>fail({$p->getExitCode()})</error>";
-                    $output->writeln("[Update <info>{$dir->getBasename()}</info>]: {$result}");
-                };
-                if ($isSwoole)
-                {
-                    go($function);
+        while (count($readyProcesses) || count($activeProcesses)) {
+            foreach ($activeProcesses as $name => $process) {
+                if (!$process->isRunning()) {
+                    $result = $process->isSuccessful() ? '<info>success</info>' : "<error>fail({$process->getExitCode()})</error>";
+                    $output->writeln("[Update <info>{$name}</info>]: {$result}");
+                    unset($activeProcesses[$name]);
                 }
-                else
-                {
-                    $function();
+
+                // check every second
+                usleep(1000 * 10);
+            }
+            foreach ($readyProcesses as $name => $process) {
+                if (count($activeProcesses) >= $maxCount) {
+                    break;
                 }
+                unset($readyProcesses[$name]);
+                $process->start(function ($type, $buffer) {
+                    echo $buffer;
+                });
+                $activeProcesses[$name] = $process;
             }
         }
-        if ($isSwoole)
+    }
+
+    protected static function createProcess(\SplFileInfo $dir): Process
+    {
+        $cmd = [
+            \PHP_BINARY,
+            realpath($_SERVER['SCRIPT_FILENAME']),
+            'update',
+            '--no-interaction',
+            '--prefer-dist',
+            '--no-progress',
+        ];
+        // 兼容 symfony process < 3.3
+        if (method_exists(Process::class, 'fromShellCommandline'))
         {
-            Event::wait();
+            $p = new Process($cmd, $dir->getPathname(), null, null, 0);
         }
+        else
+        {
+            $p = new Process([], $dir->getPathname(), null, null, 0);
+            /* @phpstan-ignore-next-line */
+            $p->setCommandLine(implode(' ', $cmd));
+        }
+//        $p->run(function ($type, $buffer) {
+//            echo $buffer;
+//        });
+
+//        $result = $p->isSuccessful() ? '<info>success</info>' : "<error>fail({$p->getExitCode()})</error>";
+//        $output->writeln("[Update <info>{$dir->getBasename()}</info>]: {$result}");
+        return $p;
     }
 }
