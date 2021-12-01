1ece7a6386e97c3f27a06570a167706fd7c23eb3
auooru <auooru@outlook.com>
2021-12-01 11:24:42 +0800

    测试jit

--- .github/script/php-jit.sh
+++ .github/script/php-jit.sh
@@ -3,10 +3,11 @@
 set -ex
 
 if [ "${PHP_JIT}" == 'yes' ]; then
+  mkdir -p /usr/local/etc/php/conf.d
   echo "opcache.enable=1
 opcache.enable_cli=1
-opcache.jit => 1205
-opcache.jit_buffer_size => 64m
+opcache.jit => 1235
+opcache.jit_buffer_size => 128m
 opcache.validate_timestamps=0
 opcache.enable_file_override=1
 opcache.huge_code_pages=1
