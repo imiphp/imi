e3c93d99254a9d143c8e3bc3bb9f3f18ca4e04ff
Yurun <admin@yurunsoft.com>
2021-12-05 16:03:11 +0800

    修复 pgsql PHP 8.1 兼容

--- src/Components/pgsql/src/Db/Drivers/PdoPgsql/Statement.php
+++ src/Components/pgsql/src/Db/Drivers/PdoPgsql/Statement.php
@@ -248,6 +248,7 @@ class Statement extends PgsqlBaseStatement implements IPgsqlStatement
     /**
      * @return mixed|false
      */
+    #[\ReturnTypeWillChange]
     public function current()
     {
         // @phpstan-ignore-next-line
@@ -257,6 +258,7 @@ class Statement extends PgsqlBaseStatement implements IPgsqlStatement
     /**
      * @return int|string|null
      */
+    #[\ReturnTypeWillChange]
     public function key()
     {
         // @phpstan-ignore-next-line
@@ -284,6 +286,7 @@ class Statement extends PgsqlBaseStatement implements IPgsqlStatement
     /**
      * {@inheritDoc}
      */
+    #[\ReturnTypeWillChange]
     public function valid()
     {
         return false !== $this->current();
