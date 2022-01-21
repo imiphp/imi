3b101cfb051bcf23dfa82ffe50c247379f9869dd
Yurun <admin@yurunsoft.com>
2022-01-21 14:05:23 +0800

    更新文档 [no ci]

--- mddoc/template/html/article.php
+++ mddoc/template/html/article.php
@@ -34,7 +34,7 @@
                     <a href="/v2.1/" target="_blank" style="padding-right: 40px;">v2.1 <span class="layui-badge">新</span></a>
                 </li>
                 <li class="layui-nav-item layui-this" lay-unselect>
-                    <a href="/v2.0/" target="_blank">v1.x</a>
+                    <a href="/v2.0/" target="_blank">v2.0</a>
                 </li>
                 <li class="layui-nav-item layui-this" lay-unselect>
                     <a href="/v1/" target="_blank">v1.x</a>
