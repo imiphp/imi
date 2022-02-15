df5396d2a01845b1661f11a39804861f054e6bc5
Yurun <admin@yurunsoft.com>
2022-02-15 11:28:14 +0800

    修正代码

--- src/Util/DocBlock.php
+++ src/Util/DocBlock.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Imi\Util;
 
+use phpDocumentor\Reflection\DocBlock as RealDocBlock;
 use phpDocumentor\Reflection\DocBlockFactory;
 use phpDocumentor\Reflection\Location;
 use phpDocumentor\Reflection\Types\Context;
@@ -29,7 +30,7 @@ class DocBlock
     /**
      * @param object|string $docblock a string containing the DocBlock to parse or an object supporting the getDocComment method (such as a ReflectionClass object)
      */
-    public static function getDocBlock($docblock, ?Context $context = null, ?Location $location = null)
+    public static function getDocBlock($docblock, ?Context $context = null, ?Location $location = null): RealDocBlock
     {
         return self::getFactory()->create($docblock, $context, $location);
     }
