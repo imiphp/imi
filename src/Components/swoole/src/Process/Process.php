4aa752b9092796fae7b3f294520fd6b95d86c12c
Yurun <admin@yurunsoft.com>
2022-03-30 15:22:15 +0800

    移除 imi-macro 强依赖

--- src/Components/swoole/src/Process/Process.php
+++ src/Components/swoole/src/Process/Process.php
@@ -1,3 +1,59 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Imi\Swoole\Process;
+
+trait TProcess
+{
+    /**
+     * 发送消息.
+     *
+     * @return mixed
+     */
+    public function sendMessage(string $action, array $data = [])
+    {
+        $data['a'] = $action;
+        $message = json_encode($data, \JSON_THROW_ON_ERROR | \JSON_UNESCAPED_SLASHES | \JSON_UNESCAPED_UNICODE);
+
+        return $this->write($message);
+    }
+
+    /**
+     * @param int|null $exitCode
+     */
+    public function exit($exitCode = 0): void
+    {
+        if ($this->pid > 0)
+        {
+            parent::exit($exitCode);
+        }
+        else
+        {
+            exit($exitCode);
+        }
+    }
+}
+
+// @phpstan-ignore-next-line
+if (\SWOOLE_VERSION_ID >= 50000)
+{
+    class Process extends \Swoole\Process
+    {
+        use TProcess {
+            exit as private __exit;
+        }
+
+        public function exit(int $exitCode = 0): void
+        {
+            $this->__exit($exitCode);
+        }
+    }
+}
+else
+{
+    class Process extends \Swoole\Process
+    {
+        use TProcess;
+    }
+}
--- src/Components/swoole/src/Util/Coroutine.php
+++ src/Components/swoole/src/Util/Coroutine.php
@@ -1,3 +1,58 @@
 <?php
 
 declare(strict_types=1);
+
+namespace Imi\Swoole\Util;
+
+trait TCoroutine
+{
+    private function __construct()
+    {
+    }
+
+    /**
+     * 判断当前是否在协程中运行.
+     */
+    public static function isIn(): bool
+    {
+        return static::getuid() > -1;
+    }
+
+    /**
+     * 创建一个新的协程，并立即执行。
+     *
+     * @param mixed ...$params
+     *
+     * @return int|bool
+     */
+    public static function create(callable $callable, ...$params)
+    {
+        return imigo($callable, ...$params);
+    }
+}
+
+// @phpstan-ignore-next-line
+if (\SWOOLE_VERSION_ID >= 50000)
+{
+    class Coroutine extends \Swoole\Coroutine
+    {
+        use TCoroutine {
+            create as private __create;
+        }
+
+        /**
+         * 创建一个新的协程，并立即执行。
+         */
+        public static function create(callable $callable, mixed ...$params): int|false
+        {
+            return self::__create($callable, ...$params);
+        }
+    }
+}
+else
+{
+    class Coroutine extends \Swoole\Coroutine
+    {
+        use TCoroutine;
+    }
+}
