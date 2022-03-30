15a8df51d0dec91ecbd2d2e3eae5dd82c0fc039e
Yurun <admin@yurunsoft.com>
2022-03-30 15:36:41 +0800

    修复

--- src/Components/swoole/src/Util/Coroutine.php
+++ src/Components/swoole/src/Util/Coroutine.php
@@ -34,20 +34,7 @@ trait TCoroutine
 // @phpstan-ignore-next-line
 if (\SWOOLE_VERSION_ID >= 50000)
 {
-    class Coroutine extends \Swoole\Coroutine
-    {
-        use TCoroutine {
-            create as private __create;
-        }
-
-        /**
-         * 创建一个新的协程，并立即执行。
-         */
-        public static function create(callable $callable, mixed ...$params): int|false
-        {
-            return self::__create($callable, ...$params);
-        }
-    }
+    include __DIR__ . '/Coroutine.swoole-5';
 }
 else
 {
