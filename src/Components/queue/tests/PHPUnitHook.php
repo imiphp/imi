6936a72f3cb519c3ef9e3c195e60f22f66a0248d
Yurun <admin@yurunsoft.com>
2022-04-01 16:15:06 +0800

    修复组件测试问题

--- src/Components/queue/tests/PHPUnitHook.php
+++ src/Components/queue/tests/PHPUnitHook.php
@@ -8,6 +8,7 @@ use Imi\App;
 use Imi\Swoole\SwooleApp;
 use PHPUnit\Runner\AfterLastTestHook;
 use PHPUnit\Runner\BeforeFirstTestHook;
+use Swoole\Coroutine;
 use Swoole\Coroutine\Channel;
 
 class PHPUnitHook implements BeforeFirstTestHook, AfterLastTestHook
@@ -17,10 +18,10 @@ class PHPUnitHook implements BeforeFirstTestHook, AfterLastTestHook
     public function executeBeforeFirstTest(): void
     {
         $this->channel = $channel = new Channel(1);
-        App::run('QueueApp', SwooleApp::class, static function () use ($channel) {
+        Coroutine::create(fn () => App::run('QueueApp', SwooleApp::class, static function () use ($channel) {
             $channel->push(1);
             $channel->pop();
-        });
+        }));
         $channel->pop();
     }
 
