b168f85cb94834cd49996b499a8a0d94092411a1
Yurun <admin@yurunsoft.com>
2022-04-22 11:03:37 +0800

    修复 Swoole BASE 模式只有一个进程时，没有输出监听端口信息

--- src/Components/swoole/src/Server/ConnectionContext/Listener/AppInit.php
+++ src/Components/swoole/src/Server/ConnectionContext/Listener/AppInit.php
@@ -7,10 +7,12 @@ namespace Imi\Swoole\Server\ConnectionContext\Listener;
 use Imi\App;
 use Imi\Bean\Annotation\Listener;
 use Imi\Event\EventParam;
+use Imi\Log\Log;
 use Imi\RequestContext;
 use Imi\Server\ServerManager;
 use Imi\Swoole\Server\Contract\ISwooleServer;
 use Imi\Swoole\Server\Event\Listener\IAppInitEventListener;
+use Imi\Swoole\Server\Traits\TServerPortInfo;
 use Imi\Util\Imi;
 
 /**
@@ -18,11 +20,20 @@ use Imi\Util\Imi;
  */
 class AppInit implements IAppInitEventListener
 {
+    use TServerPortInfo;
+
     /**
      * {@inheritDoc}
      */
     public function handle(EventParam $e): void
     {
+        /** @var ISwooleServer $server */
+        $server = ServerManager::getServer('main', ISwooleServer::class);
+        $mainSwooleServer = $server->getSwooleServer();
+        if (($serverStart = !$mainSwooleServer->manager_pid))
+        {
+            $this->outputServerInfo();
+        }
         foreach (ServerManager::getServers(ISwooleServer::class) as $server)
         {
             if ($server->isLongConnection())
@@ -36,6 +47,10 @@ class AppInit implements IAppInitEventListener
                     $groupHandler->startup();
                 }
             }
+        }
+        if ($serverStart)
+        {
+            Log::info('Server start');
         }
     }
 }
---
+++ src/Components/swoole/src/Server/Traits/TServerPortInfo.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Swoole\Server\Traits;
+
+use Imi\Cli\ImiCommand;
+use Imi\Server\ServerManager;
+use Imi\Swoole\Server\Contract\ISwooleServer;
+
+trait TServerPortInfo
+{
+    public function outputServerInfo(): void
+    {
+        $output = ImiCommand::getOutput();
+        /** @var ISwooleServer $server */
+        $server = ServerManager::getServer('main', ISwooleServer::class);
+        $mainSwooleServer = $server->getSwooleServer();
+        $output->writeln('<info>WorkerNum: </info>' . $mainSwooleServer->setting['worker_num'] . ', <info>TaskWorkerNum: </info>' . $mainSwooleServer->setting['task_worker_num']);
+        foreach (ServerManager::getServers(ISwooleServer::class) as $server)
+        {
+            /** @var ISwooleServer $server */
+            $serverPort = $server->getSwoolePort();
+            $output->writeln('<info>[' . $server->getConfig()['type'] . ']</info> <comment>' . $server->getName() . '</comment>; <info>listen:</info> ' . $serverPort->host . ':' . $serverPort->port);
+        }
+    }
+}
