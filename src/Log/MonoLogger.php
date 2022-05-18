95be927701eca48d32a13988014e094eee0ca894
Yurun <admin@yurunsoft.com>
2022-05-18 16:50:56 +0800

    兼容 monolog 2.6

--- src/Log/MonoLogger.php
+++ src/Log/MonoLogger.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace Imi\Log;
 
+use Composer\InstalledVersions;
 use Monolog\Logger;
 
 class MonoLogger extends Logger
 {
+    /**
+     * {@inheritDoc}
+     *
+     * @todo 如 monolog 支持用户接管或者配置 logDepth，请更改实现方式
+     */
+    public function __construct(...$args)
+    {
+        parent::__construct(...$args);
+        if (version_compare(InstalledVersions::getVersion('monolog/monolog'), '2.6.0', '>='))
+        {
+            $ref = new \ReflectionClass(parent::class);
+            $property = $ref->getProperty('logDepth');
+            $property->setAccessible(true);
+            $property->setValue($this, \PHP_INT_MIN);
+        }
+    }
 }
