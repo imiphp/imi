44fcebb55498ecfeb76b787bc8c9f700ab9f6a3d
Yurun <admin@yurunsoft.com>
2022-06-15 08:50:27 +0800

    优化代码

--- src/Async/Exception/AsyncTimeoutException.php
+++ src/Async/Exception/AsyncTimeoutException.php
@@ -9,7 +9,7 @@ namespace Imi\Async\Exception;
  */
 class AsyncTimeoutException extends \Exception
 {
-    public function __construct(string $message = 'Async timeout', int $code = 0, \Throwable $previous = null)
+    public function __construct(string $message = 'Async timeout', int $code = 0, ?\Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
