bc024ee58bb31951825e01a0c6a33358cb5774a5
Yurun <admin@yurunsoft.com>
2022-07-12 16:52:34 +0800

    imi-grpc 组件增加检查 Swoole http2 是否启用

--- src/Components/grpc/src/Main.php
+++ src/Components/grpc/src/Main.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace Imi\Grpc;
 
+use Imi\Log\Log;
 use Imi\Main\BaseMain;
+use Imi\Util\Imi;
 
 class Main extends BaseMain
 {
     public function __init(): void
     {
+        if (Imi::checkAppType('swoole') && !class_exists(\Swoole\Coroutine\Http2\Client::class, false))
+        {
+            Log::error('Please compile Swoole with http2: https://wiki.swoole.com/#/environment?id=-enable-http2');
+            exit(1);
+        }
     }
 }
