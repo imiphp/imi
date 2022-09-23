c9b851f100c81a959883f76bce6f2f3c2c850122
Yurun <admin@yurunsoft.com>
2022-09-23 13:39:21 +0800

    修复 Condition 注解的 callable 无法使用 ['$this', 'methodName'] 格式的值 (#436)
    * 修复 Condition 注解的 callable 无法使用 ['$this', 'methodName'] 格式的值
    
    * 修复测试

--- src/Validate/Annotation/Condition.php
+++ src/Validate/Annotation/Condition.php
@@ -15,23 +15,24 @@ use Imi\Config;
  * @Annotation
  * @Target({"CLASS", "METHOD", "PROPERTY"})
  *
- * @property string|null $name          参数名称；属性注解可省略
- * @property bool        $optional      非必验证，只有当值存在才验证
- * @property mixed       $default       当值不符合条件时的默认值
- * @property bool        $inverseResult 对结果取反
- * @property string      $message       当验证条件不符合时的信息；支持代入{:value}原始值；支持代入{:data.xxx}所有数据中的某项；支持以{name}这样的形式，代入注解参数值
- * @property callable    $callable      验证回调
- * @property array       $args          参数名数组；支持代入{:value}原始值；支持代入{:data}所有数据；支持代入{:data.xxx}所有数据中的某项；支持以{name}这样的形式，代入注解参数值；如果没有{}，则原样传值
- * @property string|null $exception     异常类
- * @property int|null    $exCode        异常编码
+ * @property string|null    $name          参数名称；属性注解可省略
+ * @property bool           $optional      非必验证，只有当值存在才验证
+ * @property mixed          $default       当值不符合条件时的默认值
+ * @property bool           $inverseResult 对结果取反
+ * @property string         $message       当验证条件不符合时的信息；支持代入{:value}原始值；支持代入{:data.xxx}所有数据中的某项；支持以{name}这样的形式，代入注解参数值
+ * @property array|callable $callable      验证回调
+ * @property array          $args          参数名数组；支持代入{:value}原始值；支持代入{:data}所有数据；支持代入{:data.xxx}所有数据中的某项；支持以{name}这样的形式，代入注解参数值；如果没有{}，则原样传值
+ * @property string|null    $exception     异常类
+ * @property int|null       $exCode        异常编码
  */
 #[\Attribute(\Attribute::TARGET_CLASS | \Attribute::TARGET_METHOD | \Attribute::TARGET_PROPERTY | \Attribute::IS_REPEATABLE)]
 class Condition extends Base
 {
     /**
-     * @param mixed $default
+     * @param mixed          $default
+     * @param array|callable $callable
      */
-    public function __construct(?array $__data = null, ?string $name = null, bool $optional = false, $default = null, bool $inverseResult = false, string $message = '{name} validate failed', ?callable $callable = null, array $args = ['{:value}'], ?string $exception = null, ?int $exCode = null)
+    public function __construct(?array $__data = null, ?string $name = null, bool $optional = false, $default = null, bool $inverseResult = false, string $message = '{name} validate failed', $callable = null, array $args = ['{:value}'], ?string $exception = null, ?int $exCode = null)
     {
         parent::__construct(...\func_get_args());
         if (null === $this->exception)
