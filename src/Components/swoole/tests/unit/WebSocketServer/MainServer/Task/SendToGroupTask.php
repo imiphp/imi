74644425f3001a8a4f9dc00ae20f45b55cc0b457
Yurun <admin@yurunsoft.com>
2022-10-08 14:51:54 +0800

    增加测试用例

---
+++ src/Components/swoole/tests/unit/WebSocketServer/MainServer/Task/SendToGroupTask.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Swoole\Test\WebSocketServer\MainServer\Task;
+
+use Imi\Server\Server;
+use Imi\Server\ServerManager;
+use Imi\Swoole\Task\Annotation\Task;
+use Imi\Swoole\Task\Interfaces\ITaskHandler;
+use Imi\Swoole\Task\TaskParam;
+
+/**
+ * @Task("SendToGroupTask")
+ */
+class SendToGroupTask implements ITaskHandler
+{
+    /**
+     * 任务处理方法.
+     *
+     * @return mixed
+     */
+    public function handle(TaskParam $param, \Swoole\Server $server, int $taskId, int $workerId)
+    {
+        $group = $param->getData()['group'];
+        $data = [
+            'data'  => 'test',
+        ];
+        $dataStr = json_encode($data);
+        $result = [
+            // @phpstan-ignore-next-line
+            'groupClientIdCount' => ServerManager::getServer('main')->getGroup($group)->count(),
+        ];
+
+        $result['sendToGroup'] = Server::sendToGroup($group, $data, 'main');
+        $result['sendRawToGroup'] = Server::sendRawToGroup($group, $dataStr, 'main');
+
+        return $result;
+    }
+
+    /**
+     * 任务结束时触发.
+     *
+     * @param mixed $data
+     */
+    public function finish(\Swoole\Server $server, int $taskId, $data): void
+    {
+    }
+}
