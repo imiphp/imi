c879a8c5de57c1b600b8956d7bb4ced303299102
Yurun <admin@yurunsoft.com>
2022-10-21 16:18:21 +0800

    修复测试

--- tests/unit/Component/Partial/Partial/TPartialClassA1.php
+++ tests/unit/Component/Partial/Partial/TPartialClassA1.php
@@ -3,36 +3,19 @@
 declare(strict_types=1);
 
 namespace Imi\Test\Component\Partial\Partial
+;
+
+use Imi\Bean\Annotation\Partial;
+
+/**
+ * @Partial(Imi\Test\Component\Partial\Classes\PartialClassA::class)
+ */
+trait TPartialClassA1
 {
-    use Imi\Bean\Annotation\Partial;
+    public int $test2Value = 2;
 
-    /**
-     * @Partial(Imi\Test\Component\Partial\Classes\PartialClassA::class)
-     */
-    trait TPartialClassA1
+    public function test2(): int
     {
-        public int $test2Value = 2;
-
-        public function test2(): int
-        {
-            return $this->test2Value;
-        }
-    }
-}
-
-namespace Imi\Test\Component\Partial\Classes
-{
-    // @phpstan-ignore-next-line
-    if (false)
-    {
-        class PartialClassA
-        {
-            public int $test2Value;
-
-            public function test2(): int
-            {
-                return 0;
-            }
-        }
+        return $this->test2Value;
     }
 }
--- tests/unit/Component/Partial/Partial/TPartialClassA2.php
+++ tests/unit/Component/Partial/Partial/TPartialClassA2.php
@@ -3,37 +3,19 @@
 declare(strict_types=1);
 
 namespace Imi\Test\Component\Partial\Partial
+;
+
+use Imi\Bean\Annotation\Partial;
+
+/**
+ * @Partial(Imi\Test\Component\Partial\Classes\PartialClassA::class)
+ */
+trait TPartialClassA2
 {
-    use Imi\Bean\Annotation\Partial;
+    private int $test3Value = 3;
 
-    /**
-     * @Partial(Imi\Test\Component\Partial\Classes\PartialClassA::class)
-     */
-    trait TPartialClassA2
+    public function test3(): int
     {
-        private int $test3Value = 3;
-
-        public function test3(): int
-        {
-            return $this->test3Value;
-        }
-    }
-}
-
-namespace Imi\Test\Component\Partial\Classes
-{
-    // @phpstan-ignore-next-line
-    if (false)
-    {
-        class PartialClassA
-        {
-            // @phpstan-ignore-next-line
-            private int $test3Value;
-
-            public function test3(): int
-            {
-                return 0;
-            }
-        }
+        return $this->test3Value;
     }
 }
