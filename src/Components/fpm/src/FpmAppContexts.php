ea4376f1a1705d404c018539a9cf32639da77de6
Yurun <admin@yurunsoft.com>
2022-11-01 15:20:28 +0800

    优化 fpm 的路由初始化触发逻辑

---
+++ src/Components/fpm/src/FpmAppContexts.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Fpm;
+
+class FpmAppContexts
+{
+    /**
+     * 进程类型.
+     */
+    public const ROUTE_INITED = 'fpm_route_inited';
+
+    private function __construct()
+    {
+    }
+}
