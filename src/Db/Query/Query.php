20f9078522630c9e72e118898b9537d1906b49fc
Yurun <admin@yurunsoft.com>
2022-12-14 13:17:42 +0800

    优化分页查询，统计记录数量时移除排序提升性能

--- src/Db/Query/Query.php
+++ src/Db/Query/Query.php
@@ -775,14 +775,15 @@ abstract class Query implements IQuery
     {
         if ($options['total'] ?? true)
         {
-            $total = (int) (clone $this)->count();
+            $query = (clone $this);
+            $query->option->order = [];
+            $total = (int) $query->count();
         }
         else
         {
             $total = null;
         }
-        $this->page($page, $count);
-        $statement = $this->select();
+        $statement = $this->page($page, $count)->select();
         $pagination = new Pagination($page, $count);
 
         return new PaginateResult($statement, $pagination->getLimitOffset(), $count, $total, null === $total ? null : $pagination->calcPageCount($total), $options);
