32e54c602e5fb4b2153bfe41e31da8e0c807b9cf
Yurun <admin@yurunsoft.com>
2023-03-03 13:04:13 +0800

    修复定时任务指定 force=true 后，启动服务会执行两次 (#474)
    * 修复定时任务指定 force=true 后，启动服务会执行两次
    
    * 优化代码
    
    * 修复
    # Conflicts:
    #	src/Components/swoole/src/Cron/Scheduler.php
    #	src/Cron/Scheduler.php

--- src/Components/swoole/src/Cron/Scheduler.php
+++ src/Components/swoole/src/Cron/Scheduler.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 
 namespace Imi\Swoole\Cron;
 
-use Imi\Aop\Annotation\Inject;
 use Imi\App;
 use Imi\Bean\Annotation\Bean;
-use Imi\Cron\Contract\ICronManager;
-use Imi\Cron\Contract\IScheduler;
-use Imi\Cron\CronCalculator;
-use Imi\Cron\CronLock;
 use Imi\Cron\CronTask;
-use Imi\Cron\Message\Result;
 use Imi\Log\Log;
 use Imi\Server\ServerManager;
 use Imi\Swoole\Cron\Consts\CronTaskType;
@@ -26,23 +20,8 @@ use Yurun\Swoole\CoPool\Interfaces\ITaskParam;
  *
  * 定时任务调度器
  */
-class Scheduler implements IScheduler
+class Scheduler extends \Imi\Cron\Scheduler
 {
-    /**
-     * @Inject("CronManager")
-     */
-    protected ICronManager $cronManager;
-
-    /**
-     * @Inject("CronCalculator")
-     */
-    protected CronCalculator $cronCalculator;
-
-    /**
-     * @Inject("CronLock")
-     */
-    protected CronLock $cronLock;
-
     /**
      * 协程工作池的协程数量.
      */
@@ -58,25 +37,9 @@ class Scheduler implements IScheduler
      */
     private CoPool $coPool;
 
-    /**
-     * 下次执行时间集合.
-     */
-    private array $nextTickTimeMap = [];
-
-    /**
-     * 正在执行的任务列表.
-     *
-     * @var \Imi\Cron\CronTask[]
-     */
-    private array $runningTasks = [];
-
-    /**
-     * 首次执行记录集合.
-     */
-    private array $firstRunMap = [];
-
     public function __construct()
     {
+        parent::__construct();
         $this->coPool = $coPool = new CoPool($this->poolCoCount, $this->poolQueueLength,
             // 定义任务匿名类，当然你也可以定义成普通类，传入完整类名
             // @phpstan-ignore-next-line
@@ -146,48 +109,8 @@ class Scheduler implements IScheduler
      */
     public function close(): void
     {
+        parent::close();
         $this->coPool->stop();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function schedule(): \Generator
-    {
-        $now = time();
-        $runningTasks = &$this->runningTasks;
-        $nextTickTimeMap = &$this->nextTickTimeMap;
-        $cronCalculator = $this->cronCalculator;
-        $firstRunMap = &$this->firstRunMap;
-        foreach ($this->cronManager->getRealTasks() as $task)
-        {
-            $id = $task->getId();
-            if (isset($runningTasks[$id]))
-            {
-                if (time() < $task->getLastRunTime() + $task->getMaxExecutionTime())
-                {
-                    continue;
-                }
-                else
-                {
-                    unset($runningTasks[$id]);
-                }
-            }
-            $nextTickTimeMap[$id] ??= $cronCalculator->getNextTickTime($task->getLastRunTime(), $task->getCronRules());
-            $firstRun = !isset($firstRunMap[$id]) && $task->getForce();
-            if ($firstRun || $now >= $nextTickTimeMap[$id])
-            {
-                if ($firstRun)
-                {
-                    $firstRunMap[$id] = true;
-                }
-                else
-                {
-                    unset($nextTickTimeMap[$id]);
-                }
-                yield $task;
-            }
-        }
     }
 
     /**
@@ -204,30 +127,5 @@ class Scheduler implements IScheduler
         $task->updateLastRunTime(time());
         $this->runningTasks[$task->getId()] = $task;
         $this->coPool->addTaskAsync($task);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function completeTask(Result $result): void
-    {
-        $runningTasks = &$this->runningTasks;
-        $resultId = $result->id;
-        if (isset($runningTasks[$resultId]))
-        {
-            if (!$this->cronLock->unlock($runningTasks[$resultId]))
-            {
-                Log::error(sprintf('Task %s unlock failed', $resultId));
-            }
-            unset($runningTasks[$resultId]);
-        }
-        if ($result->success)
-        {
-            Log::info(sprintf('Task: %s, Process: %s#%s, Success', $resultId, $result->processType, $result->processId));
-        }
-        else
-        {
-            Log::error(sprintf('Task: %s, Process: %s#%s, %s', $resultId, $result->processType, $result->processId, $result->message));
-        }
     }
 }
