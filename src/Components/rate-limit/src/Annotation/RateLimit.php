1a3fc4df45e7e6d1462c401f94cd60b0d0c2c427
Yurun <admin@yurunsoft.com>
2023-04-23 14:27:51 +0800

    修复限流器注解 RateLimit fill 参数默认值

--- src/Components/rate-limit/src/Annotation/RateLimit.php
+++ src/Components/rate-limit/src/Annotation/RateLimit.php
@@ -15,7 +15,7 @@ use Imi\Bean\Annotation\Base;
  *
  * @property string        $name     限流器名称
  * @property int           $capacity 总容量
- * @property int           $fill     单位时间内生成填充的数量；不设置或为null时，默认值与 $capacity 相同
+ * @property int|null      $fill     单位时间内生成填充的数量；不设置或为null时，默认值与 $capacity 相同
  * @property string        $unit     单位时间，默认为：秒(second)；支持：microsecond、millisecond、second、minute、hour、day、week、month、year
  * @property int           $deduct   每次扣除数量，默认为1
  * @property callable|null $callback 触发限流的回调
@@ -24,7 +24,7 @@ use Imi\Bean\Annotation\Base;
 #[\Attribute(\Attribute::TARGET_METHOD)]
 class RateLimit extends Base
 {
-    public function __construct(?array $__data = null, string $name = '', int $capacity = 0, int $fill = 0, string $unit = 'second', int $deduct = 1, ?callable $callback = null, ?string $poolName = null)
+    public function __construct(?array $__data = null, string $name = '', int $capacity = 0, ?int $fill = null, string $unit = 'second', int $deduct = 1, ?callable $callback = null, ?string $poolName = null)
     {
         parent::__construct(...\func_get_args());
     }
