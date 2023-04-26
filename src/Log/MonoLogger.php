3d682619a05832c3548d709e60ac458d021b615c
auooru <auooru@outlook.com>
2023-04-26 21:12:43 +0800

    改进兼容实现方式

--- src/Log/MonoLogger.php
+++ src/Log/MonoLogger.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Imi\Log;
 
+use Composer\InstalledVersions;
 use Monolog\Logger;
 
 class MonoLogger extends Logger
@@ -18,6 +19,16 @@ class MonoLogger extends Logger
         parent::__construct(...$args);
 
         // 禁用不兼容协程并发的深度检测功能
-        $this->useLoggingLoopDetection(false);
+        if (method_exists($this, 'useLoggingLoopDetection'))
+        {
+            $this->useLoggingLoopDetection(false);
+        }
+        elseif (version_compare(InstalledVersions::getVersion('monolog/monolog'), '2.6.0', '>='))
+        {
+            $ref = new \ReflectionClass(parent::class);
+            $property = $ref->getProperty('logDepth');
+            $property->setAccessible(true);
+            $property->setValue($this, \PHP_INT_MIN);
+        }
     }
 }
