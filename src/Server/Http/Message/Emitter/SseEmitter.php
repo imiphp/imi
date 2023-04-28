b75c92f392a157d7425dabb141f287a2ddd6690d
Yurun <admin@yurunsoft.com>
2023-04-28 16:39:49 +0800

    补充响应头

--- src/Server/Http/Message/Emitter/SseEmitter.php
+++ src/Server/Http/Message/Emitter/SseEmitter.php
@@ -17,7 +17,11 @@ abstract class SseEmitter extends BaseEmitter
     public function init(IHttpResponse &$response, IEmitHandler $handler): void
     {
         $this->handler = $handler;
-        $response->setHeader(ResponseHeader::CONTENT_TYPE, MediaType::TEXT_EVENT_STREAM);
+        $response->setHeader(ResponseHeader::CONTENT_TYPE, MediaType::TEXT_EVENT_STREAM)
+                 ->setHeader(ResponseHeader::CACHE_CONTROL, 'no-cache')
+                 ->setHeader(ResponseHeader::CONNECTION, 'Keep-Alive')
+                 ->setHeader('X-Accel-Buffering', 'no') // 禁用 Nginx 缓冲区
+        ;
     }
 
     public function getHandler(): IEmitHandler
