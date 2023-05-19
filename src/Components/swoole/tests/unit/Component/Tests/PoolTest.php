a4cd49867d476a2f40db851791a90f9e91ea4fd9
Yurun <admin@yurunsoft.com>
2023-05-19 16:05:12 +0800

    修复测试

--- src/Components/swoole/tests/unit/Component/Tests/PoolTest.php
+++ src/Components/swoole/tests/unit/Component/Tests/PoolTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Imi\Swoole\Test\Component\Tests;
 
-use Imi\App;
+use Imi\RequestContext;
 use Imi\Swoole\Db\Pool\CoroutineDbPool;
 use Imi\Swoole\Test\BaseTest;
 use Imi\Swoole\Test\Component\Pool\PoolTestClass;
@@ -13,7 +13,9 @@ class PoolTest extends BaseTest
 {
     public function testPoolResource(): void
     {
-        $object = App::getBean(PoolTestClass::class);
-        $this->assertInstanceOf(CoroutineDbPool::class, $object->maindbPool);
+        $this->go(function () {
+            $object = RequestContext::getBean(PoolTestClass::class);
+            $this->assertInstanceOf(CoroutineDbPool::class, $object->maindbPool);
+        });
     }
 }
