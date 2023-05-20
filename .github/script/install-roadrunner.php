b318de9a8992269ed5782c6a3ed4f8afd3eab7ae
Yurun <admin@yurunsoft.com>
2023-05-20 09:37:19 +0800

    修复测试

--- .github/script/install-roadrunner.php
+++ .github/script/install-roadrunner.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 $argv = $_SERVER['argv'];
 unset($argv[0]);
 
-$cmd = \dirname(__DIR__, 2) . 'src/Components/roadrunner/vendor/bin/rr get-binary ' . implode(' ', $argv);
+$cmd = \dirname(__DIR__, 2) . '/src/Components/roadrunner/vendor/bin/rr get-binary ' . implode(' ', $argv);
 
 $count = 12;
 for ($i = 0; $i < $count; ++$i)
