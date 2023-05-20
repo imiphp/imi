95178cabf0d5bc4ea696c1237e0893aa9cdbda45
Yurun <admin@yurunsoft.com>
2023-05-20 09:24:41 +0800

    修复 roadrunner 并发测试过多时测试失败

---
+++ .github/script/install-roadrunner.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+$argv = $_SERVER['argv'];
+unset($argv[0]);
+
+$cmd = \dirname(__DIR__, 2) . 'src/Components/roadrunner/vendor/bin/rr get-binary ' . implode(' ', $argv);
+
+$count = 12;
+for ($i = 0; $i < $count; ++$i)
+{
+    passthru($cmd, $code);
+    if (0 === $code)
+    {
+        return;
+    }
+    sleep(10);
+}
+exit($code);
