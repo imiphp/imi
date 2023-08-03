895a64f8a5186ce193d7d8083b778a63442622bd
Yurun <admin@yurunsoft.com>
2023-08-03 11:24:00 +0800

    修复

--- src/Db/Mysql/Query/Pagination/BigTablePagination.php
+++ src/Db/Mysql/Query/Pagination/BigTablePagination.php
@@ -8,7 +8,6 @@ use Imi\Db\Query\Interfaces\IPaginateResult;
 use Imi\Db\Query\Interfaces\IQuery;
 use Imi\Db\Query\Interfaces\IResult;
 use Imi\Db\Query\PaginateResult;
-use Imi\Db\Query\WrapField;
 use Imi\Util\Pagination;
 
 class BigTablePagination
@@ -48,17 +47,7 @@ class BigTablePagination
             $query = clone $this->query;
             $option = $query->getOption();
             $option->order = [];
-            if ($option->distinct)
-            {
-                $option->field = [
-                    new WrapField('count(distinct ', $option->field ?: ['*'], ')'),
-                ];
-                $total = (int) $query->select()->getScalar();
-            }
-            else
-            {
-                $total = (int) $query->count();
-            }
+            $total = (int) $query->count();
         }
         else
         {
@@ -73,33 +62,10 @@ class BigTablePagination
     public function select(int $page, int $limit): IResult
     {
         $query = clone $this->query;
-        $option = $query->getOption();
-        if ($option->distinct)
-        {
-            $list = $query->field($this->idField)
-                          ->page($page, $limit)
-                          ->select()
-                          ->getArray();
-            var_dump($list);
-            if ($list)
-            {
-                $row = $list[0];
-                end($row);
-                $ids = array_column($list, key($row));
-            }
-            else
-            {
-                $ids = [];
-            }
-        }
-        else
-        {
-            $option->field = [];
-            $ids = $query->field($this->idField)
-                         ->page($page, $limit)
-                         ->select()
-                         ->getColumn();
-        }
+        $ids = $query->field($this->idField)
+                     ->page($page, $limit)
+                     ->select()
+                     ->getColumn();
 
         $query = clone $this->query;
         $option = $query->getOption();
