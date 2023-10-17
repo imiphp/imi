acebe138e7fcbb618acc836a04645fea8b8f8b93
auooru <auooru@outlook.com>
2023-10-18 02:04:45 +0800

    Update: 组件规则更新

---
+++ phpstan-v2.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+__DIR__=$(cd `dirname $0`; pwd)
+cd $__DIR__
+
+components=(
+  "core"
+  "access-control"
+  "amqp"
+  "apidoc"
+  "fpm"
+  "grpc"
+  "jwt"
+  "kafka"
+  "mqtt"
+  "pgsql"
+  "queue"
+  "rate-limit"
+  "roadrunner"
+  "rpc"
+  "shared-memory"
+  "smarty"
+  "snowflake"
+  "swoole"
+  "swoole-tracker"
+  "workerman"
+  "workerman-gateway"
+  "macro"
+)
+
+analyze_component() {
+  component="$1"
+  gen_baseline="$2"
+  echo "Analyzing $component"
+
+  analyse_configuration=""
+
+  args=()
+  args+=("--memory-limit" "1G")
+
+  if [ "$component" != "core" ]; then
+    args+=("--configuration=phpstan-components.neon" "--autoload-file=src/Components/$component/vendor/autoload.php" "src/Components/$component")
+  fi
+
+  if [ "$gen_baseline" == "true" ]; then
+    args+=("--generate-baseline=./phpstan-baseline/baseline-$component.neon", "--allow-empty-baseline")
+  fi
+
+  set -x
+  ./vendor/bin/phpstan analyse "${args[@]}"
+  set +x > /dev/null
+}
+
+gen_baseline="false"
+input_components=()
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --baseline)
+      use_baseline="true"
+      shift
+      ;;
+    -b)
+      use_baseline="true"
+      shift
+      ;;
+    *)
+      # 如果不是 --baseline 标志，将参数添加到 components 数组中
+      input_components+=("$1")
+      shift
+      ;;
+  esac
+done
+
+if [ ${#input_components[@]} -eq 0 ]; then
+  # If no arguments are provided, analyze all components
+  for component in "${components[@]}"; do
+    analyze_component "$component" "$gen_baseline"
+  done
+else
+  # Analyze the specified components provided as arguments
+  for component in "${input_components[@]}"; do
+    if [[ " ${components[@]} " =~ " $component " ]]; then
+      analyze_component "$component" "$gen_baseline"
+    else
+      echo "Invalid component name: $component"
+    fi
+  done
+fi
