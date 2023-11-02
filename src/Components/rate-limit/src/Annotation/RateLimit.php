99ed42b02984d78009dd9b9da4d6eb158b414103
Yurun <admin@yurunsoft.com>
2023-11-02 14:39:11 +0800

    rate-limit

--- src/Components/rate-limit/src/Annotation/RateLimit.php
+++ src/Components/rate-limit/src/Annotation/RateLimit.php
@@ -8,24 +8,41 @@ use Imi\Bean\Annotation\Base;
 
 /**
  * 限流器注解.
- *
- * @Annotation
- *
- * @Target("METHOD")
- *
- * @property string        $name     限流器名称
- * @property int           $capacity 总容量
- * @property int|null      $fill     单位时间内生成填充的数量；不设置或为null时，默认值与 $capacity 相同
- * @property string        $unit     单位时间，默认为：秒(second)；支持：microsecond、millisecond、second、minute、hour、day、week、month、year
- * @property int           $deduct   每次扣除数量，默认为1
- * @property callable|null $callback 触发限流的回调
- * @property string|null   $poolName 连接池名称，留空取默认 redis 连接池
  */
 #[\Attribute(\Attribute::TARGET_METHOD)]
 class RateLimit extends Base
 {
-    public function __construct(?array $__data = null, string $name = '', int $capacity = 0, ?int $fill = null, string $unit = 'second', int $deduct = 1, ?callable $callback = null, ?string $poolName = null)
-    {
-        parent::__construct(...\func_get_args());
+    public function __construct(
+        /**
+         * 限流器名称.
+         */
+        public string $name = '',
+        /**
+         * 总容量.
+         */
+        public int $capacity = 0,
+        /**
+         * 单位时间内生成填充的数量；不设置或为null时，默认值与 $capacity 相同.
+         */
+        public ?int $fill = null,
+        /**
+         * 单位时间，默认为：秒(second)；支持：microsecond、millisecond、second、minute、hour、day、week、month、year.
+         */
+        public string $unit = 'second',
+        /**
+         * 每次扣除数量，默认为1.
+         */
+        public int $deduct = 1,
+        /**
+         * 触发限流的回调.
+         *
+         * @var ?callable
+         */
+        public $callback = null,
+        /**
+         * 连接池名称，留空取默认 redis 连接池.
+         */
+        public ?string $poolName = null
+    ) {
     }
 }
