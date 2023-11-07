e0ef003431e41b2d554dcf127668927c0424863b
auooru <auooru@outlook.com>
2023-11-07 20:14:19 +0800

    Update: 使 phpstan 缓存生效

--- phpstan-baseline/component-baseline.php
+++ phpstan-baseline/component-baseline.php
@@ -5,16 +5,22 @@ declare(strict_types=1);
 $component = getenv('PHPSTAN_ANALYSE_COMPONENT_NAME');
 $generateBaseline = getenv('PHPSTAN_GENERATE_BASELINE');
 
+$defaultConfig = [
+    'parameters' => [
+        'resultCachePath' => "%tmpDir%/resultCache-imi-3-{$component}.php",
+    ],
+];
+
 if (empty($component) || 'true' === $generateBaseline)
 {
-    return [];
+    return $defaultConfig;
 }
 
 $file = __DIR__ . "/baseline-{$component}.neon";
 
 if (!file_exists($file))
 {
-    return [];
+    return $defaultConfig;
 }
 
 // echo $file, PHP_EOL;
@@ -23,4 +29,7 @@ use PHPStan\DependencyInjection\NeonAdapter;
 
 $adapter = new NeonAdapter();
 
-return $adapter->load($file);
+$config = $adapter->load($file);
+$config['parameters']['resultCachePath'] = $defaultConfig['parameters']['resultCachePath'];
+
+return $config;
