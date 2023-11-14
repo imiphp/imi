743dccd9161e813e72f7d921a88c3648415d2a10
Yurun <admin@yurunsoft.com>
2023-11-14 15:17:04 +0800

    重构 Imi\Db\Mysql\Consts\LogicalOperator

--- src/Db/Mysql/Consts/LogicalOperator.php
+++ src/Db/Mysql/Consts/LogicalOperator.php
@@ -4,28 +4,19 @@ declare(strict_types=1);
 
 namespace Imi\Db\Mysql\Consts;
 
-use Imi\Enum\Annotation\EnumItem;
-use Imi\Enum\BaseEnum;
-
-class LogicalOperator extends BaseEnum
+class LogicalOperator
 {
     use \Imi\Util\Traits\TStaticClass;
 
-    #[EnumItem]
     public const AND = 'and';
 
-    #[EnumItem]
     public const OR = 'or';
 
-    #[EnumItem]
     public const XOR = 'xor';
 
-    #[EnumItem]
     public const AND_NOT = 'and not';
 
-    #[EnumItem]
     public const OR_NOT = 'or not';
 
-    #[EnumItem]
     public const XOR_NOT = 'xor not';
 }
