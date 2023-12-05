dd9347d3bf331c6d246bfa3f676d9d68761a8192
Yurun <admin@yurunsoft.com>
2023-12-05 13:30:47 +0800

    标记废弃 Imi\Util\Format\PhpSession

--- tests/unit/Component/Tests/Util/Format/PhpSessionTest.php
+++ tests/unit/Component/Tests/Util/Format/PhpSessionTest.php
@@ -9,4 +9,12 @@ use Imi\Util\Format\PhpSession;
 class PhpSessionTest extends BaseFormatTest
 {
     protected string $class = PhpSession::class;
+
+    protected function setUp(): void
+    {
+        if (\PHP_VERSION_ID >= 80300)
+        {
+            $this->markTestSkipped();
+        }
+    }
 }
