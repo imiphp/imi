d2d827410b7bedd4f6a515ca918aa39d1600518d
阿诺Eno <895183594@qq.com>
2025-07-03 11:30:22 +0800

    修复MySQL8下更新JSON字段时出现的字符转换问题
    PDOException: SQLSTATE[22032]: <<Unknown error>>: 3144 Cannot create a JSON value from a string with CHARACTER SET 'binary'.

--- src/Db/Mysql/Query/Builder/UpdateBuilder.php
+++ src/Db/Mysql/Query/Builder/UpdateBuilder.php
@@ -73,7 +73,7 @@ class UpdateBuilder extends BaseBuilder
                     {
                         $jsonSets[$field][] = [
                             'jsonKeywords' => $matches['jsonKeywords'],
-                            'raw'          => 'CONVERT(\'' . json_encode($v, \JSON_THROW_ON_ERROR | \JSON_UNESCAPED_SLASHES | \JSON_UNESCAPED_UNICODE) . '\',JSON)',
+                            'raw'          => 'CAST(\'' . json_encode($v, \JSON_THROW_ON_ERROR | \JSON_UNESCAPED_SLASHES | \JSON_UNESCAPED_UNICODE) . '\' AS JSON)',
                         ];
                     }
                 }
